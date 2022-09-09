f9429dd0a6953db0edac9500c5ab645fed7bec6e
Stephen Dicks <github@starswan.com>
2022-09-09 08:14:12 +0100

    Add coverage tools to Circle:CI
    Add simplecov coverage tool with threshold set to 100% coverage. This will help us make sure that we don't forget to test any scenarios

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -4,8 +4,8 @@ ENV["RAILS_ENV"] ||= "test"
 require_relative "../config/environment"
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
-require "rspec/rails"
 # Add additional requires below this line. Rails is not loaded until this point!
+require "rspec/rails"
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
@@ -30,6 +30,7 @@ rescue ActiveRecord::PendingMigrationError => e
   puts e.to_s.strip
   exit 1
 end
+
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   # config.fixture_path = "#{::Rails.root}/spec/fixtures"
