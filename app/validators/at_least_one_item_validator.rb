61edec2d38bccbbe1593087612b4563d3477eebc
Stephen Dicks <github@starswan.com>
2022-09-12 10:37:36 +0100

    Rubocop-govuk and ERB lint checks
    Add rubocop-govuk and erb-lint tools to Circle:CI toolchain to
    assist with code consistency and avoiding known defect patterns

--- app/validators/at_least_one_item_validator.rb
+++ app/validators/at_least_one_item_validator.rb
@@ -1,9 +1,9 @@
 class AtLeastOneItemValidator < ActiveModel::EachValidator
   # If the 'exclusive' option is picked, then no items are sent
   # otherwise we should get at least 2 (a blank plus at least one selected)
-  def validate_each record, attribute, value
+  def validate_each(record, attribute, value)
     if value.size == 1
-      record.errors.add(attribute, :at_least_one_item, **options.merge(value: value))
+      record.errors.add(attribute, :at_least_one_item, **options.merge(value:))
     end
   end
 end
