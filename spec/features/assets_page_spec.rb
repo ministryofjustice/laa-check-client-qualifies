278e875d129ac75a8b390bfbed65cc4ae21b2d90
Stephen Dicks <github@starswan.com>
2022-09-26 13:48:20 +0100

    Bug: assets form can't ask second home question if no first home owned

--- spec/features/assets_page_spec.rb
+++ spec/features/assets_page_spec.rb
@@ -14,60 +14,82 @@ RSpec.describe "Assets Page", :vcr do
     select_applicant_boolean(:employed, false)
     select_applicant_boolean(:passporting, true)
     click_on "Save and continue"
-
-    click_checkbox("property-form-property-owned", "with_mortgage")
-    click_on "Save and continue"
-
-    fill_in "property-entry-form-house-value-field", with: 100_000
-    fill_in "property-entry-form-mortgage-field", with: 50_000
-    fill_in "property-entry-form-percentage-owned-field", with: 100
-    click_on "Save and continue"
-
-    select_boolean_value("vehicle-form", :vehicle_owned, false)
-    click_on "Save and continue"
   end
 
-  it "shows the correct page" do
-    expect(page).to have_content assets_header
-  end
+  context "with no property" do
+    before do
+      click_checkbox("property-form-property-owned", "none")
+      click_on "Save and continue"
 
-  it "sets error on assets form" do
-    click_on "Save and continue"
-    expect(page).to have_css(".govuk-error-summary__list")
-    within ".govuk-error-summary__list" do
-      expect(page).to have_content("Please select at least one option")
+      select_boolean_value("vehicle-form", :vehicle_owned, false)
+      click_on "Save and continue"
+    end
+
+    it "shows the correct page" do
+      expect(page).to have_content assets_header
+    end
+
+    it "doesnt show second property question" do
+      expect(page).not_to have_content "holiday home"
     end
   end
 
-  it "can submit second property" do
-    click_checkbox("assets-form-assets", "property")
-    fill_in "assets-form-property-value-field", with: "100_000"
-    fill_in "assets-form-property-mortgage-field", with: "50_000"
-    fill_in "assets-form-property-percentage-owned-field", with: "50"
-    click_on "Save and continue"
+  context "with a mortgage" do
+    before do
+      click_checkbox("property-form-property-owned", "with_mortgage")
+      click_on "Save and continue"
 
-    expect(page).to have_content "Summary Page"
-  end
+      fill_in "property-entry-form-house-value-field", with: 100_000
+      fill_in "property-entry-form-mortgage-field", with: 50_000
+      fill_in "property-entry-form-percentage-owned-field", with: 100
+      click_on "Save and continue"
 
-  it "can submit non-zero savings and investments" do
-    click_checkbox("assets-form-assets", "savings")
-    fill_in "assets-form-savings-field", with: "100"
+      select_boolean_value("vehicle-form", :vehicle_owned, false)
+      click_on "Save and continue"
+    end
 
-    click_checkbox("assets-form-assets", "investments")
-    fill_in "assets-form-investments-field", with: "500"
+    it "shows the correct page" do
+      expect(page).to have_content assets_header
+    end
 
-    click_on "Save and continue"
+    it "sets error on assets form" do
+      click_on "Save and continue"
+      expect(page).to have_css(".govuk-error-summary__list")
+      within ".govuk-error-summary__list" do
+        expect(page).to have_content("Please select at least one option")
+      end
+    end
 
-    expect(page).to have_content "Summary Page"
-  end
+    it "can submit second property" do
+      click_checkbox("assets-form-assets", "property")
+      fill_in "assets-form-property-value-field", with: "100_000"
+      fill_in "assets-form-property-mortgage-field", with: "50_000"
+      fill_in "assets-form-property-percentage-owned-field", with: "50"
+      click_on "Save and continue"
 
-  it "can fill in the assets questions and get to results" do
-    click_checkbox("assets-form-assets", "none")
-    click_on "Save and continue"
+      expect(page).to have_content "Summary Page"
+    end
 
-    expect(page).to have_content "Summary Page"
-    click_on "Submit"
+    it "can submit non-zero savings and investments" do
+      click_checkbox("assets-form-assets", "savings")
+      fill_in "assets-form-savings-field", with: "100"
 
-    expect(page).to have_content "Results Page"
+      click_checkbox("assets-form-assets", "investments")
+      fill_in "assets-form-investments-field", with: "500"
+
+      click_on "Save and continue"
+
+      expect(page).to have_content "Summary Page"
+    end
+
+    it "can fill in the assets questions and get to results" do
+      click_checkbox("assets-form-assets", "none")
+      click_on "Save and continue"
+
+      expect(page).to have_content "Summary Page"
+      click_on "Submit"
+
+      expect(page).to have_content "Results Page"
+    end
   end
 end
