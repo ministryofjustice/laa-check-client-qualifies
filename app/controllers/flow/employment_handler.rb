daadc17e88bc7cc23a6547a3e54315026affa1e9
Patrick Gleeson <patrick.gleeson@digital.justice.gov.uk>
2022-10-14 12:05:47 +0100

    Adjust number of "payslips" sent to CFE

--- app/controllers/flow/employment_handler.rb
+++ app/controllers/flow/employment_handler.rb
@@ -12,13 +12,13 @@ module Flow
 
       def save_data(cfe_connection, estimate_id, form, _session_data)
         # CFE wants to infer frequency of payment from gaps between payments.
-        # So we use our knowledge of frequency to generate three appropriately-spaced,
+        # So we use our knowledge of frequency to generate several appropriately-spaced,
         # representative payments, to allow CFE to make that inference
         employment_data = [
           {
             name: "Job",
             client_id: "ID",
-            payments: Array.new(3) do |index|
+            payments: Array.new(number_of_payments(form)) do |index|
               {
                 gross: (form.gross_income * multiplier(form)).round(2),
                 tax: (-1 * form.income_tax * multiplier(form)).round(2),
@@ -36,6 +36,20 @@ module Flow
       end
 
     private
+
+      # CFE expects to receive 12 payment instances if the payment frequency is weekly,
+      # and 6 if it is fortnightly. If it does not receive that number it will not recognise
+      # those frequencies. For 4-weekly or monthly it will accept 3 payments
+      def number_of_payments(form)
+        case form.frequency
+        when "week"
+          12
+        when "two_weeks"
+          6
+        else
+          3
+        end
+      end
 
       # CFE doesn't understand about annual salary or 'total income in the last 3 months',
       # so for both those use cases we must convert the figures we have into what they would
