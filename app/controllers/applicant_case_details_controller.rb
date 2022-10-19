90ac90406cd472851280f0c8ab6bb4fee31e6a73
Stephen Dicks <github@starswan.com>
2022-10-19 17:26:57 +0100

    removed duplicate code

--- app/controllers/applicant_case_details_controller.rb
+++ app/controllers/applicant_case_details_controller.rb
@@ -6,21 +6,6 @@ class ApplicantCaseDetailsController < EstimateFlowController
     applicant: Flow::ApplicantHandler,
   }.freeze
 
-  def update
-    handler = HANDLER_CLASSES.fetch(step)
-    @form = handler.form(params, session_data)
-
-    if @form.valid?
-      session_data.merge!(@form.attributes)
-      handler.save_data(cfe_connection, estimate_id, @form, session_data)
-
-      redirect_to next_wizard_path
-    else
-      @estimate = load_estimate
-      render_wizard
-    end
-  end
-
   def finish_wizard_path
     estimate_build_estimate_path estimate_id, params[:build_estimate_id]
   end
