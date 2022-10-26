9be8d47748a556f88c42e71c3bf69434eb17fa9b
willc-work <william.clarke@digital.justice.gov.uk>
2022-10-26 16:52:06 +0100

    reinstate check

--- app/services/submit_irregular_income_service.rb
+++ app/services/submit_irregular_income_service.rb
@@ -6,24 +6,26 @@ class SubmitIrregularIncomeService < BaseCfeService
   def call(cfe_estimate_id, cfe_session_data)
     form = Flow::MonthlyIncomeHandler.model(cfe_session_data)
 
-    if form.student_finance
+    if form.monthly_incomes.include?("student_finance")
       create_student_loan cfe_connection, cfe_estimate_id, form.student_finance
     end
 
-    if form.other
+    if form.monthly_incomes.include?("other")
       create_other_income cfe_connection, cfe_estimate_id, form.other
     end
   end
 
   def create_student_loan(cfe_connection, assessment_id, amount)
-    payments = [
-      {
-        "income_type": "student_loan",
-        "frequency": "annual",
-        "amount": amount,
-      },
-    ]
-    cfe_connection.create_irregular_income(assessment_id, payments:)
+    if amount.present?
+      payments = [
+        {
+          "income_type": "student_loan",
+          "frequency": "annual",
+          "amount": amount,
+        },
+      ]
+      cfe_connection.create_irregular_income(assessment_id, payments:)
+    end
   end
 
   def create_other_income(cfe_connection, assessment_id, amount)
