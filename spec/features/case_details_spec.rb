0bb31b648fbe70369ea31be6ac53e5365ce0678a
Stephen Dicks <github@starswan.com>
2022-11-03 13:39:30 +0000

    EL-391 Reword first domestic abuse case type question (#148)

--- spec/features/case_details_spec.rb
+++ spec/features/case_details_spec.rb
@@ -8,6 +8,10 @@ RSpec.describe "Case Details Page" do
     visit new_estimate_path
   end
 
+  it "has the correct content" do
+    expect(page).to have_content "Yes"
+  end
+
   it "can submit a domestic abuse case" do
     click_checkbox("proceeding-type-form-proceeding-type", "da001")
     click_on "Save and continue"
@@ -17,7 +21,7 @@ RSpec.describe "Case Details Page" do
   it "has readable errors" do
     click_on "Save and continue"
     within ".govuk-error-summary__list" do
-      expect(page).to have_content("Please select a case type")
+      expect(page).to have_content("Select yes if your client is likely to be an applicant in a domestic abuse case")
     end
   end
 end
