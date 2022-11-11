3567547cdc02d67e8f1155fbef7ac4be13cb8830
Patrick Gleeson <patrick.gleeson@digital.justice.gov.uk>
2022-11-11 16:53:20 +0000

    Alternative wording for dependants pages when client has partner

--- spec/features/dependants_pages_spec.rb
+++ spec/features/dependants_pages_spec.rb
@@ -19,40 +19,66 @@ RSpec.describe "Dependants" do
   before do
     travel_to arbitrary_fixed_time
     allow(CfeConnection).to receive(:connection).and_return(mock_connection)
-    visit estimate_build_estimate_path(estimate_id, :dependants)
   end
 
-  it "checks I have made a choice" do
-    click_on("Save and continue")
-    expect(page).to have_content("Select yes if the client has any dependants")
-  end
-
-  it "allows me to skip past the details screen" do
-    select_boolean_value("dependants-form", :dependants, false)
-    click_on("Save and continue")
-    expect(page).to have_content(benefits_page_header)
-  end
-
-  it "requires me to enter dependants if I say I have them" do
-    select_boolean_value("dependants-form", :dependants, true)
-    click_on "Save and continue"
-    click_on "Save and continue"
-    expect(page).to have_content("Please enter a zero if the client has no child dependants")
-    expect(page).to have_content("Please enter a zero if the client has no adult dependants")
-  end
-
-  it "allows me to enter dependant numberss and passes them on to CFE" do
-    select_boolean_value("dependants-form", :dependants, true)
-    click_on "Save and continue"
-    fill_in "Adult dependants", with: "1"
-    fill_in "Child dependants", with: "2"
-    click_on "Save and continue"
-
-    expect(mock_connection).to receive(:create_dependants) do |_estimate_id, params|
-      expect(params.count { _1[:date_of_birth] < 18.years.ago }).to eq 1
-      expect(params.count { _1[:date_of_birth] > 18.years.ago }).to eq 2
+  context "when the client has no partner" do
+    before do
+      visit estimate_build_estimate_path(estimate_id, :dependants)
     end
 
-    progress_to_submit_from_benefits
+    it "shows the non-partner text" do
+      expect(page).to have_content "Does your client have any dependants?"
+    end
+
+    it "checks I have made a choice" do
+      click_on("Save and continue")
+      expect(page).to have_content("Select yes if the client has any dependants")
+    end
+
+    it "allows me to skip past the details screen" do
+      select_boolean_value("dependants-form", :dependants, false)
+      click_on("Save and continue")
+      expect(page).to have_content(benefits_page_header)
+    end
+
+    it "requires me to enter dependants if I say I have them" do
+      select_boolean_value("dependants-form", :dependants, true)
+      click_on "Save and continue"
+      click_on "Save and continue"
+      expect(page).to have_content("Please enter a zero if the client has no child dependants")
+      expect(page).to have_content("Please enter a zero if the client has no adult dependants")
+    end
+
+    it "allows me to enter dependant numberss and passes them on to CFE" do
+      select_boolean_value("dependants-form", :dependants, true)
+      click_on "Save and continue"
+      expect(page).to have_content "Tell us about your client's dependants"
+      fill_in "Adult dependants", with: "1"
+      fill_in "Child dependants", with: "2"
+      click_on "Save and continue"
+
+      expect(mock_connection).to receive(:create_dependants) do |_estimate_id, params|
+        expect(params.count { _1[:date_of_birth] < 18.years.ago }).to eq 1
+        expect(params.count { _1[:date_of_birth] > 18.years.ago }).to eq 2
+      end
+
+      progress_to_submit_from_benefits
+    end
+  end
+
+  context "when the client has a partner" do
+    before do
+      visit estimate_build_estimate_path(estimate_id, :partner)
+      select_boolean_value("partner-form", :partner, true)
+      click_on("Save and continue")
+      visit estimate_build_estimate_path(estimate_id, :dependants)
+    end
+
+    it "shows the partner text" do
+      expect(page).to have_content "Does your client and/or their partner have any dependants?"
+      select_boolean_value("dependants-form", :dependants, true)
+      click_on("Save and continue")
+      expect(page).to have_content "Tell us about your client and their partner's dependants"
+    end
   end
 end
