a1ab2b782343d29d8d6317b94a80ee8a3dc8a0b0
willc-work <william.clarke@digital.justice.gov.uk>
2022-11-22 11:44:52 +0000

    rebase

--- app/services/submit_applicant_service.rb
+++ app/services/submit_applicant_service.rb
@@ -1,6 +1,6 @@
 class SubmitApplicantService < BaseCfeService
   def call(cfe_estimate_id, cfe_session_data)
-    form = ApplicantForm.from_session(cfe_session_data)
+    estimate = ApplicantForm.from_session(cfe_session_data)
 
     applicant = {
       date_of_birth: estimate.over_60 ? 70.years.ago.to_date : 50.years.ago.to_date,
