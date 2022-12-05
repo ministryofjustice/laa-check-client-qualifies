b6a3fbe47136e0cab6fd684441a712c143c43682
Patrick Gleeson <patrick.gleeson@digital.justice.gov.uk>
2022-12-05 14:35:17 +0000

    Only submit employment data if applicant is employed

--- app/services/submit_employment_income_service.rb
+++ app/services/submit_employment_income_service.rb
@@ -1,8 +1,9 @@
 class SubmitEmploymentIncomeService < BaseCfeService
   def call(cfe_estimate_id, cfe_session_data)
-    form = EmploymentForm.from_session(cfe_session_data)
-    return if form.gross_income.blank?
+    applicant_form = ApplicantForm.from_session(cfe_session_data)
+    return unless applicant_form.employed
 
+    form = EmploymentForm.from_session(cfe_session_data)
     employment_data = CfeParamBuilders::Employments.call(form)
 
     cfe_connection.create_employment(cfe_estimate_id, employment_data)
