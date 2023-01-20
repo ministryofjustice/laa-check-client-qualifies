c60083972acb81a95d5539a0ec8912b96e838f39
willc-work <william.clarke@digital.justice.gov.uk>
2023-01-20 10:59:43 +0000

    use secure cookies

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -33,7 +33,7 @@ Rails.application.configure do
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # This should not be set in our k8s environment
-  # config.force_ssl = true
+  config.force_ssl = true
 
   # Include generic and useful information about system operation, but avoid logging too much
   # information to avoid inadvertent exposure of personally identifiable information (PII).
