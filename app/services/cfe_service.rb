c65a2833da19b802d1374d14570d73f588bd0172
willc-work <william.clarke@digital.justice.gov.uk>
2023-01-25 16:32:38 +0000

    test speed

--- app/services/cfe_service.rb
+++ app/services/cfe_service.rb
@@ -1,25 +1,6 @@
 class CfeService
   class << self
-    def call(cfe_session_data)
-      puts ">>>>>>>>>  #{__FILE__}:#{__LINE__} <<<<<<<<<<"
-      puts 111111
-      cfe_connection = CfeConnection.connection
-      puts ">>>>>>>>>  #{__FILE__}:#{__LINE__} <<<<<<<<<<"
-      puts 222222
-      cfe_estimate_id = cfe_connection.create_assessment_id
-      puts ">>>>>>>>>  #{__FILE__}:#{__LINE__} <<<<<<<<<<"
-      puts 333333333
-      Async do |task|
-        SERVICES.each do |service|
-          task.async { service.call(cfe_connection, cfe_estimate_id, cfe_session_data) }
-        end
-      end
-      puts ">>>>>>>>>  #{__FILE__}:#{__LINE__} <<<<<<<<<<"
-      puts 5555
-      cfe_connection.api_result(cfe_estimate_id)
-    end
-
-    SERVICES = [
+    CFE_SERVICES = [
       SubmitDependantsService,
       SubmitProceedingsService,
       SubmitEmploymentIncomeService,
@@ -31,16 +12,16 @@ class CfeService
       SubmitApplicantService,
       SubmitPartnerService,
     ].freeze
+
+    def call(cfe_session_data)
+      cfe_connection = CfeConnection.connection
+      cfe_estimate_id = cfe_connection.create_assessment_id
+      Async do |task|
+        CFE_SERVICES.each do |service|
+          task.async { service.call(cfe_connection, cfe_estimate_id, cfe_session_data) }
+        end
+      end
+      cfe_connection.api_result(cfe_estimate_id)
+    end
   end
 end
-
-# SubmitDependantsService.call(cfe_connection, cfe_estimate_id, cfe_session_data)
-# SubmitProceedingsService.call(cfe_connection, cfe_estimate_id, cfe_session_data)
-# SubmitEmploymentIncomeService.call(cfe_connection, cfe_estimate_id, cfe_session_data)
-# SubmitBenefitsService.call(cfe_connection, cfe_estimate_id, cfe_session_data)
-# SubmitIrregularIncomeService.call(cfe_connection, cfe_estimate_id, cfe_session_data)
-# SubmitVehicleService.call(cfe_connection, cfe_estimate_id, cfe_session_data)
-# SubmitAssetsService.call(cfe_connection, cfe_estimate_id, cfe_session_data)
-# SubmitRegularTransactionsService.call(cfe_connection, cfe_estimate_id, cfe_session_data)
-# SubmitApplicantService.call(cfe_connection, cfe_estimate_id, cfe_session_data)
-# SubmitPartnerService.call(cfe_connection, cfe_estimate_id, cfe_session_data)
--- app/services/submit_proceedings_service.rb
+++ app/services/submit_proceedings_service.rb
@@ -2,5 +2,6 @@ class SubmitProceedingsService < BaseCfeService
   def call(cfe_estimate_id, cfe_session_data)
     form = ApplicantForm.from_session(cfe_session_data)
     cfe_connection.create_proceeding_type(cfe_estimate_id, form.proceeding_type)
+    #  make this condtional on certificated/controlled to reduce calls if possible
   end
 end
