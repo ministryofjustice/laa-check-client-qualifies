7d539fcfb8de9e88d438b297d7a8d7ecae07f83c
Stephen Dicks <github@starswan.com>
2023-02-16 13:10:00 +0000

    EL-686 Replace date with level of help in sidebar (#418)

---
+++ app/helpers/assets_helper.rb
@@ -0,0 +1,16 @@
+module AssetsHelper
+  def assets_links(level_of_help)
+    first_links = {
+      t("estimate_flow.assets.guidance.#{level_of_help}.text") => t("estimate_flow.assets.guidance.#{level_of_help}.link"),
+      t("estimate_flow.assets.other_property_guidance.text") => t("estimate_flow.assets.other_property_guidance.#{level_of_help}.link"),
+    }
+    links = if level_of_help == "controlled"
+              first_links
+            else
+              first_links.merge({
+                t("estimate_flow.assets.disregarded_capital_guidance.text") => t("estimate_flow.assets.disregarded_capital_guidance.certificated.link"),
+              })
+            end
+    links.merge({ t("generic.smod.guidance.text") => t("generic.smod.guidance.#{level_of_help}.link") })
+  end
+end
