d240c4308e433efd5c61264a65a560b75620a876
Patrick Gleeson <patrick.gleeson@digital.justice.gov.uk>
2023-02-21 09:26:59 +0000

    Fix bug when controlled flag not enabled

--- app/services/submit_proceedings_service.rb
+++ app/services/submit_proceedings_service.rb
@@ -2,7 +2,7 @@ class SubmitProceedingsService < BaseCfeService
   def call(cfe_assessment_id, session_data)
     applicant_form = ApplicantForm.from_session(session_data)
     tribunal_form = TribunalForm.from_session(session_data)
-    proceeding_type = if applicant_form.level_of_help == "certificated"
+    proceeding_type = if applicant_form.level_of_help != "controlled"
                         applicant_form.proceeding_type
                       elsif tribunal_form.upper_tribunal
                         matter_type_form = MatterTypeForm.from_session(session_data)
