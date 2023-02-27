5c92557290208c1882cc631c0cc3711652f021bb
Patrick Gleeson <patrick.gleeson@digital.justice.gov.uk>
2023-02-27 12:26:38 +0000

    UAT comments

---
+++ app/helpers/asylum_support_helper.rb
@@ -0,0 +1,14 @@
+module AsylumSupportHelper
+  def asylum_support_links(level_of_help)
+    if level_of_help == "controlled"
+      {
+        t("estimate_flow.asylum_support.guidance.text") => t("estimate_flow.asylum_support.guidance.controlled_link"),
+        t("estimate_flow.tribunal.guidance_text") => t("estimate_flow.tribunal.guidance_link"),
+      }
+    else
+      {
+        t("estimate_flow.asylum_support.guidance.text") => t("estimate_flow.asylum_support.guidance.certificated_link"),
+      }
+    end
+  end
+end
