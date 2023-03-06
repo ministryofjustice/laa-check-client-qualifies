f8af92e2b6446c802630d21d23b466904d340414
Patrick Gleeson <patrick.gleeson@digital.justice.gov.uk>
2023-03-06 14:05:20 +0000

    Fix rebase issues

--- app/services/submit_vehicle_service.rb
+++ app/services/submit_vehicle_service.rb
@@ -2,8 +2,8 @@ class SubmitVehicleService < BaseCfeService
   def call(cfe_assessment_id)
     return unless relevant_form?(:vehicle_details)
 
-    details_model = ClientVehicleDetailsForm.from_session(@session_data)
+    details_model = VehicleDetailsForm.from_session(@session_data)
     vehicles = CfeParamBuilders::Vehicles.call(details_model)
-    cfe_connection.create_vehicle cfe_assessment_id, vehicles:
+    cfe_connection.create_vehicles cfe_assessment_id, vehicles:
   end
 end
