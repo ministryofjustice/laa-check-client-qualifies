dd842c05b8d2e07c4b07ccbdcfdb9c27d76acf90
Patrick Gleeson <113888736+patrick-laa@users.noreply.github.com>
2023-03-10 14:12:54 +0000

    Property tables have SMOD and % owned (#496)

--- app/helpers/results_helper.rb
+++ app/helpers/results_helper.rb
@@ -1,8 +1,8 @@
 module ResultsHelper
-  def header_with_hint(header_key, hint_key)
+  def header_with_hint(header_key, hint_key, hint_args: {})
     (
       I18n.t(header_key) +
-        tag.div(I18n.t(hint_key),
+        tag.div(I18n.t(hint_key, **hint_args),
                 class: "govuk-hint govuk-!-margin-top-1 govuk-!-margin-bottom-0")
     ).html_safe
   end
