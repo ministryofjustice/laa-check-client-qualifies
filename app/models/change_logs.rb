752efe75501febd50d14b593f01731b0a578127e
Patrick Gleeson <patrick.gleeson@digital.justice.gov.uk>
2023-03-23 10:25:11 +0000

    Add change log logic

---
+++ app/models/change_logs.rb
@@ -0,0 +1,19 @@
+class ChangeLogs
+  CHANGES = [
+    { key: :dependant_allowance, show_from: "2023-4-4", change_on: "2023-4-10", show_until: "2023-5-4" },
+  ].freeze
+
+  class << self
+    def for_banner
+      CHANGES.select { Time.current.beginning_of_day >= _1[:show_from] && Time.current.beginning_of_day <= _1[:show_until] }
+    end
+
+    def latest_change
+      occurred.map { Date.parse(_1[:change_on]) }.max.strftime("%-d %B %Y")
+    end
+
+    def occurred
+      CHANGES.select { Time.current.beginning_of_day >= _1[:change_on] }
+    end
+  end
+end
