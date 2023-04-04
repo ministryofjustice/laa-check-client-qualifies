c3a895aa4d86ffa784478ba9ca5fad01b9cda1e0
Masum Khan <masum.khan@digital.justice.gov.uk>
2023-04-04 15:57:02 +0100

    abandoned test, which checked  'to_f' CFE call

--- spec/services/cfe/submit_employment_income_service_spec.rb
+++ spec/services/cfe/submit_employment_income_service_spec.rb
@@ -49,5 +49,54 @@ RSpec.describe Cfe::SubmitEmploymentIncomeService do
         described_class.call(mock_connection, cfe_assessment_id, session_data)
       end
     end
+
+    # context "when there is a full set of data, which includes tax/income tax/gross income/national insurance as a string including a trailing white space" do
+    #   let(:session_data) do
+    #     {
+    #       "employment_status" => "in_work",
+    #       "frequency" => "monthly",
+    #       "gross_income" => "122 ", # this is converted to a float in employments.rb
+    #       "national_insurance" => "25 ", # this is converted to a float in employments.rb
+    #       "income_tax" => "18 ", # this is converted to a float in employments.rb
+    #     }
+    #   end
+
+    #   it "calls CFE with tax/income tax/gross income/national insurance converted to a float" do
+    #     expect(mock_connection).to receive(:create_employments).with(
+    #       cfe_assessment_id,
+    #       client_id: "ID",
+    #       name: "Job",
+    #       payments: [{
+    #         gross: 122.0,
+    #         tax: -18.0,
+    #         national_insurance: -25.0,
+    #         client_id: "id-0",
+    #         date: Date.today,
+    #         benefits_in_kind: 0,
+    #         net_employment_income: 79.0,
+    #       },
+    #                  {
+    #                    gross: 122.0,
+    #                    tax: -18.0,
+    #                    national_insurance: -25.0,
+    #                    client_id: "id-1",
+    #                    date: Time.zone.today - 1.month,
+    #                    benefits_in_kind: 0,
+    #                    net_employment_income: 79.0,
+    #                  },
+    #                  {
+    #                    gross: 122.0,
+    #                    tax: -18.0,
+    #                    national_insurance: -25.0,
+    #                    client_id: "id-2",
+    #                    date: Time.zone.today - 2.months,
+    #                    benefits_in_kind: 0,
+    #                    net_employment_income: 79.0,
+    #                  }],
+    #       receiving_only_statutory_sick_or_maternity_pay: false,
+    #     )
+    #     described_class.call(mock_connection, cfe_assessment_id, session_data)
+    #   end
+    # end
   end
 end
