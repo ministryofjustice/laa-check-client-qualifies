37dcaedde05def2ac4533db1652af5060fee09b7
Patrick Gleeson <patrick.gleeson@digital.justice.gov.uk>
2023-04-11 11:38:34 +0100

    More nuance

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -2,7 +2,8 @@ class SessionController < ApplicationController
   DUMMY_DATA = {
     "string" => "foo",
     "integer" => 1,
-    "decimal" => 1.23,
+    "float" => 1.23,
+    "decimal" => 1.23.to_d,
   }.freeze
 
   def populate
@@ -16,6 +17,6 @@ class SessionController < ApplicationController
     standard = session["standard"]
     json = JSON.parse(session["json"])
     xml = Hash.from_trusted_xml(session["xml"])["hash"]
-    render plain: "standard:<br/> #{standard}<br/> json: <br/>#{json}<br/> xml: <br/> #{xml}"
+    render inline: "standard:<br/> #{standard}<br/> json: <br/>#{json}<br/> xml: <br/> #{xml}"
   end
 end
