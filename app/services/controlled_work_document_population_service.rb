48be4081f7d5ec81807415bca203f4da93ed8a97
Patrick Gleeson <patrick.gleeson@digital.justice.gov.uk>
2023-04-11 15:36:09 +0100

    Step 2: Dynamic form population mechanism through config

--- app/services/controlled_work_document_population_service.rb
+++ app/services/controlled_work_document_population_service.rb
@@ -1,24 +1,47 @@
 class ControlledWorkDocumentPopulationService
   class << self
-    def call(session_data, document_choice_form)
+    def call(session_data, form_type)
       Dir.mktmpdir do |dir|
         file_name = "#{dir}/output-form.pdf"
         pdftk = PdfForms.new(`which pdftk`.chomp)
-        pdftk.fill_form template_path(document_choice_form), file_name, values(session_data, document_choice_form)
+        pdftk.fill_form template_path(form_type), file_name, values(session_data, form_type)
         yield File.read(file_name).force_encoding("BINARY")
       end
     end
 
-    def template_path(document_choice_form)
-      # TODO: Support other form types
-      raise "#{document_choice_form.form_type} not yet supported" unless document_choice_form.form_type == "cw1"
+    TEMPLATES = {
+      "cw1" => "lib/cw1-form.pdf",
+    }.freeze
 
-      Rails.root.join("lib/cw1-form.pdf")
+    def template_path(form_type)
+      Rails.root.join(TEMPLATES.fetch(form_type))
     end
 
-    def values(_session_data, _document_choice_form)
-      # TODO: Populate real data dynamically
-      { "undefined_88": "This field is called undefined_88" }
+    def values(session_data, form_type)
+      check = Check.new(session_data)
+      calculation_result = session_data["api_response"]
+      mappings = YAML.load_file(Rails.root.join("app/lib/controlled_work_mappings/#{form_type}.yml")).with_indifferent_access
+      mappings[:mappings].map { [_1[:name], convert(_1, check, calculation_result)] }.to_h
+    end
+
+    def convert(mapping, check, calculation_result)
+      case mapping[:type]
+      when "checkbox"
+        mapping[:checked_value] if value(mapping, check, calculation_result)
+      when "text"
+        value(mapping, check, calculation_result)
+      end
+    end
+
+    def value(mapping, check, calculation_result)
+      case mapping[:source]
+      when "answers"
+        check.send(mapping[:attribute])
+      when "result"
+        calculation_result.dig(*mapping[:attribute].split("."))
+      when "static"
+        mapping[:name]
+      end
     end
   end
 end
