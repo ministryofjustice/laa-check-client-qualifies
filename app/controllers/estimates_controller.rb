2c6da2da41bb8fe461e0ede0b5784080bf47235c
Masum Khan <masum.khan@digital.justice.gov.uk>
2023-04-17 13:22:46 +0100

    changes made to layout

--- app/controllers/estimates_controller.rb
+++ app/controllers/estimates_controller.rb
@@ -44,8 +44,12 @@ class EstimatesController < ApplicationController
         left: "1cm",
         right: "1cm",
       },
+      timezone: "Europe/London",
+      vision_deficiency: "deuteranopia",
       emulate_media: "print",
-      launch_args: ["--no-sandbox"],
+      launch_args: ["--font-render-hinting=medium", "--lang=en-GB", "--no-sandbox"],
+      extra_http_headers: { "Accept-Language": "en_GB" },
+      geolocation: { latitude: 51.49969, longitude: -0.134728 },
       display_url: request.url.split("/estimates").first,
     }
 
