80c14fe3ed921a5e7f2c3fc2a31824b4034aad99
Patrick Gleeson <patrick.gleeson@digital.justice.gov.uk>
2023-04-20 11:33:16 +0100

    Ensure vehicle SMOD is never nil

--- app/services/cfe_param_builders/vehicles.rb
+++ app/services/cfe_param_builders/vehicles.rb
@@ -7,7 +7,7 @@ module CfeParamBuilders
           loan_amount_outstanding: form.vehicle_pcp ? form.vehicle_finance : 0,
           date_of_purchase: form.vehicle_over_3_years_ago ? 4.years.ago.to_date : 2.years.ago.to_date,
           in_regular_use: form.vehicle_in_regular_use,
-          subject_matter_of_dispute: in_dispute,
+          subject_matter_of_dispute: in_dispute || false,
         },
       ]
     end
