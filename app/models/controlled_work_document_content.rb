8e96fbc0c4219837badb484cc407cc3f04d613fb
Masum Khan <masum.khan@digital.justice.gov.uk>
2023-04-21 11:41:46 +0100

    Last bits of mapping and tests
    more mappings & an addtional method

--- app/models/controlled_work_document_content.rb
+++ app/models/controlled_work_document_content.rb
@@ -38,6 +38,10 @@ private
     house_in_dispute || vehicle_in_dispute || in_dispute.present?
   end
 
+  def no_smod_assets?
+    house_in_dispute || vehicle_in_dispute || in_dispute.present? == true ? false : true
+  end
+
   def additional_property_in_dispute?
     in_dispute.include? "property"
   end
