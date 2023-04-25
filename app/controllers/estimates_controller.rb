bb9888e97101e4d7ab025faa17cad1dba7e5b49d
MazOneTwoOne <76905544+MazOneTwoOne@users.noreply.github.com>
2023-04-25 14:22:51 +0100

    PDF text goes left to right & uses gds-transport

--- app/controllers/estimates_controller.rb
+++ app/controllers/estimates_controller.rb
@@ -44,9 +44,10 @@ class EstimatesController < ApplicationController
         left: "1cm",
         right: "1cm",
       },
-      emulate_media: "print",
+      emulate_media: "screen",
       launch_args: ["--font-render-hinting=medium", "--no-sandbox"],
       display_url: request.url.split("/estimates").first,
+      execute_script: "document.querySelector('button').style.display = 'none'",
     }
 
     pdf = Grover.new(html, **grover_options).to_pdf
