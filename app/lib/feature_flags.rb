84795ce141c8b09be705276fc64189bc1169d1eb
Patrick Gleeson <113888736+patrick-laa@users.noreply.github.com>
2023-04-28 15:33:30 +0100

    EL-820: Feature flags endpoint (#617)
    * Feature flags endpoint
    
    * Rename spec
    
    * Add accessibility spec

--- app/lib/feature_flags.rb
+++ app/lib/feature_flags.rb
@@ -1,7 +1,7 @@
 class FeatureFlags
   ENABLED_AFTER_DATE = {
-    example_2125_flag: "2125-01-01",
-    dependance_allowance_increase: "2023-04-10",
+    example_2125_flag: { from: "2125-01-01", public: false },
+    dependance_allowance_increase: { from: "2023-04-10", public: true },
   }.freeze
 
   FLIPPER_FLAGS = %i[sentry cw_forms household_section special_applicant_groups].freeze
@@ -9,12 +9,20 @@ class FeatureFlags
   class << self
     def enabled?(flag)
       if ENABLED_AFTER_DATE.key?(flag)
-        Time.current.beginning_of_day >= ENABLED_AFTER_DATE[flag]
+        Time.current.beginning_of_day >= ENABLED_AFTER_DATE.dig(flag, :from)
       elsif FLIPPER_FLAGS.include?(flag)
         Flipper.enabled?(flag)
       else
         raise "Unrecognised flag '#{flag}'"
       end
+    end
+
+    def time_dependant
+      ENABLED_AFTER_DATE.select { |_, properties| properties[:public] }.map { |flag, _| flag }
+    end
+
+    def static
+      FLIPPER_FLAGS
     end
   end
 end
