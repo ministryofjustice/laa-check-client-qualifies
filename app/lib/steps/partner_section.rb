7c421ce519729fc0ec700825d4043ea27f818178
Patrick Gleeson <patrick.gleeson@digital.justice.gov.uk>
2023-05-12 11:59:41 +0100

    Update section ID finder

--- app/lib/steps/partner_section.rb
+++ app/lib/steps/partner_section.rb
@@ -5,6 +5,10 @@ module Steps
         %i[partner_details partner_dependant_details]
       end
 
+      def all_steps_for_current_feature_flags
+        all_steps
+      end
+
       def grouped_steps_for(session_data)
         if !Steps::Logic.partner?(session_data)
           []
