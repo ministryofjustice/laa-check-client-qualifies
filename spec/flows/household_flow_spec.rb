78b596d292e41946a4aa06f5db9a99d8b88ba728
MazOneTwoOne <76905544+MazOneTwoOne@users.noreply.github.com>
2023-05-18 18:13:52 +0100

    getting test coverage to 100%

--- spec/flows/household_flow_spec.rb
+++ spec/flows/household_flow_spec.rb
@@ -27,6 +27,30 @@ RSpec.describe "Household section flow", :household_section_flag, type: :feature
     confirm_screen("check_answers")
   end
 
+  it "visits the appropriate screens, if property is owned outright" do
+    start_assessment
+    fill_in_forms_until(:applicant)
+    fill_in_applicant_screen(partner: "Yes")
+    fill_in_forms_until(:outgoings)
+    fill_in_outgoings_screen
+    # property screen is skipped
+    confirm_screen("vehicle")
+    fill_in_forms_until(:partner_outgoings)
+    fill_in_partner_outgoings_screen
+    # partner property screen is skipped
+    # partner vehicle screen is skipped
+    confirm_screen("partner_assets")
+    fill_in_partner_assets_screen
+    confirm_screen("property")
+    fill_in_property_screen(choice: "Yes, owned outright")
+    fill_in_property_entry_screen
+    fill_in_additional_property_screen(choice: "Yes, owned outright")
+    fill_in_additional_property_details_screen
+    fill_in_partner_additional_property_screen(choice: "Yes, owned outright")
+    fill_in_partner_additional_property_details_screen
+    confirm_screen("check_answers")
+  end
+
   it "skips property if the client is asylum supported" do
     start_assessment
     fill_in_forms_until(:matter_type)
