2a922a59bef54ed17d558a1cd1a5e7559e5a7150
MazOneTwoOne <76905544+MazOneTwoOne@users.noreply.github.com>
2023-05-18 16:16:20 +0100

    test refactor

--- app/helpers/housing_costs_helper.rb
+++ app/helpers/housing_costs_helper.rb
@@ -1,5 +1,5 @@
 module HousingCostsHelper
-  def outgoings_links(level_of_help)
+  def housing_costs_links(level_of_help)
     {
       t("estimate_flow.housing_costs.guidance_on_housing_costs.#{level_of_help}.text") =>
         t("estimate_flow.housing_costs.guidance_on_housing_costs.#{level_of_help}.link"),
--- app/helpers/mortgage_or_loan_payment_helper.rb
+++
@@ -1,8 +0,0 @@
-module MortgageOrLoanPaymentHelper
-  def outgoings_links(level_of_help)
-    {
-      t("estimate_flow.mortgage_or_loan_payment.guidance_on_housing_costs.#{level_of_help}.text") =>
-        t("estimate_flow.mortgage_or_loan_payment.guidance_on_housing_costs.#{level_of_help}.link"),
-    }
-  end
-end
