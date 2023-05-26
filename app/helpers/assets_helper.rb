fbd22a0148aa49488f00ca0245f5e7eb6a7273ef
Patrick Gleeson <113888736+patrick-laa@users.noreply.github.com>
2023-05-26 14:41:28 +0100

    EL-901 & EL-902: Special applicants assets (#693)
    * Special applicant assets content
    
    * Review comments

--- app/helpers/assets_helper.rb
+++ app/helpers/assets_helper.rb
@@ -1,19 +1,35 @@
 module AssetsHelper
   def assets_links(level_of_help, smod_applicable)
-    first_links = {
+    links = {
       t("estimate_flow.assets.guidance.#{level_of_help}.text") => t("estimate_flow.assets.guidance.#{level_of_help}.link"),
       t("estimate_flow.assets.other_property_guidance.text") => t("estimate_flow.assets.other_property_guidance.#{level_of_help}.link"),
     }
-    links = if level_of_help == "controlled"
-              first_links
-            else
-              first_links.merge({
-                t("estimate_flow.assets.disregarded_capital_guidance.text") => t("estimate_flow.assets.disregarded_capital_guidance.link"),
-              })
-            end
+
+    if level_of_help == "certificated"
+      links[t("estimate_flow.assets.disregarded_capital_guidance.text")] = t("estimate_flow.assets.disregarded_capital_guidance.link")
+
+      if FeatureFlags.enabled?(:special_applicant_groups)
+        links[t("estimate_flow.assets.prisoner_guidance.text")] = t("estimate_flow.assets.prisoner_guidance.link")
+        links[t("estimate_flow.assets.bankrupt_guidance.text")] = t("estimate_flow.assets.bankrupt_guidance.link")
+      end
+    end
 
     return links unless smod_applicable
 
     links.merge({ t("generic.smod.guidance.text") => t("generic.smod.guidance.#{level_of_help}.link") })
+  end
+
+  def partner_assets_links(check)
+    links = {
+      t("estimate_flow.assets.guidance.#{check.level_of_help}.text") => t("estimate_flow.assets.guidance.#{check.level_of_help}.link"), \
+      t("estimate_flow.assets.other_property_guidance.text") => t("estimate_flow.assets.other_property_guidance.#{check.level_of_help}.link"), \
+    }
+
+    return links unless FeatureFlags.enabled?(:special_applicant_groups) && !check.controlled?
+
+    links.merge(
+      t("estimate_flow.partner_assets.prisoner_guidance.text") => t("estimate_flow.assets.prisoner_guidance.link"),
+      t("estimate_flow.partner_assets.bankrupt_guidance.text") => t("estimate_flow.assets.bankrupt_guidance.link"),
+    )
   end
 end
