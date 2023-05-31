5d6b4d3aad489ff51c2217d4a00077eb9e8533c3
willc-work <william.clarke@digital.justice.gov.uk>
2023-05-31 12:04:15 +0100

    separate updates from banners

--- app/models/change_logs.rb
+++ app/models/change_logs.rb
@@ -1,12 +1,17 @@
 class ChangeLogs
-  CHANGES = [
-    { key: :dependant_allowance, show_from: "2023-4-4", change_on: "2023-4-10", show_until: "2023-5-4" },
-    { key: :household_flow, show_from: "2023-5-29", change_on: "2023-5-30", show_until: "2023-5-29" },
+  UPDATES = [
+    { key: :household_flow, change_on: "2023-6-6" },
   ].freeze
 
+  BANNERS = [
+    { key: :dependant_allowance, change_on: "2023-4-10", show_from: "2023-4-4", show_until: "2023-5-4" },
+  ].freeze
+
+  CHANGE_LOG = UPDATES + BANNERS
+
   class << self
-    def for_banner
-      CHANGES.select { Time.current.beginning_of_day >= _1[:show_from] && Time.current.beginning_of_day <= _1[:show_until] }
+    def for_banner_display
+      BANNERS.select { Time.current.beginning_of_day >= _1[:show_from] && Time.current.beginning_of_day <= _1[:show_until] }
     end
 
     def last_updated_at
@@ -14,7 +19,7 @@ class ChangeLogs
     end
 
     def occurred
-      CHANGES.select { Time.current.beginning_of_day >= _1[:change_on] }
+      CHANGE_LOG.select { Time.current.beginning_of_day >= _1[:change_on] }
     end
   end
 end
