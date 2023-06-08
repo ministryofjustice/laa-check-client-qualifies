230bea5379b59cc76a1003d89a6063e74ec55c88
Patrick Gleeson <patrick.gleeson@digital.justice.gov.uk>
2023-06-08 10:29:43 +0100

    Fix bug and add tests

--- spec/services/cfe/regular_transactions_payload_service_spec.rb
+++ spec/services/cfe/regular_transactions_payload_service_spec.rb
@@ -166,6 +166,36 @@ RSpec.describe Cfe::RegularTransactionsPayloadService do
         end
       end
 
+      context "when client does not own their home but has no housing costs" do
+        let(:session_data) do
+          {
+            "property_owned" => "none",
+            "housing_payments" => 0,
+          }
+        end
+
+        it "does not include rent or mortgage in the payload" do
+          service.call(session_data, payload)
+          expect(payload[:regular_transactions]).to eq(
+            [],
+          )
+        end
+      end
+
+      context "when there is an invalid frequency" do
+        let(:session_data) do
+          {
+            "property_owned" => "none",
+            "housing_payments" => 1,
+            "housing_payments_frequency" => "invalid",
+          }
+        end
+
+        it "raises an error" do
+          expect { service.call(session_data, payload) }.to raise_error("key not found: \"invalid\"")
+        end
+      end
+
       context "when client or their partner do own their home with mortgage or loan" do
         let(:session_data) do
           {
