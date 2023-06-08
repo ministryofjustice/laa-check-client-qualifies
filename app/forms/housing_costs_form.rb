4cec517c07781d4cf2db7772d7dae93f28382528
Patrick Gleeson <113888736+patrick-laa@users.noreply.github.com>
2023-06-08 15:38:20 +0100

    Validate benefits do not exceed costs (#721)

--- app/forms/housing_costs_form.rb
+++ app/forms/housing_costs_form.rb
@@ -24,6 +24,8 @@ class HousingCostsForm
             inclusion: { in: BenefitModel::FREQUENCY_OPTIONS, allow_nil: false },
             if: -> { housing_benefit_value.to_i.positive? }
 
+  validate :housing_benefit_does_not_exceed_costs
+
   delegate :level_of_help, :partner, to: :check
 
   def frequencies
@@ -33,5 +35,37 @@ class HousingCostsForm
   def housing_payment_frequencies
     valid_frequencies = level_of_help == "controlled" ? OutgoingsForm::VALID_FREQUENCIES - %w[total] : OutgoingsForm::VALID_FREQUENCIES
     valid_frequencies.map { [_1, I18n.t("estimate_flow.outgoings.frequencies.#{_1}")] }
+  end
+
+private
+
+  def housing_benefit_does_not_exceed_costs
+    return unless housing_payments.to_i.positive? &&
+      housing_benefit_value.to_i.positive? &&
+      housing_payments_frequency.present? &&
+      housing_benefit_frequency.present?
+
+    annual_housing_payment_value = housing_payments * annual_multiplier(housing_payments_frequency)
+    annual_housing_benefit_value = housing_benefit_value * annual_multiplier(housing_benefit_frequency)
+    return if annual_housing_payment_value >= annual_housing_benefit_value
+
+    errors.add(:housing_benefit_value, :exceeds_costs)
+  end
+
+  def annual_multiplier(frequency)
+    case frequency
+    when "every_week"
+      52
+    when "every_two_weeks"
+      26
+    when "every_four_weeks"
+      13
+    when "monthly"
+      12
+    when "total"
+      4
+    else
+      raise "Unrecognised frequency: #{frequency}"
+    end
   end
 end
