a71827ee6eeb7a485c96b6dda1b99b51cc122fef
Patrick Gleeson <patrick.gleeson@digital.justice.gov.uk>
2023-06-22 15:02:44 +0100

    Conditionally show self-employment content on assets page

--- app/forms/partner_income_form.rb
+++ app/forms/partner_income_form.rb
@@ -1,6 +1,6 @@
 class PartnerIncomeForm < IncomeForm
   SESSION_KEY = "partner_incomes".freeze
-
+  PREFIX = "partner_".freeze
   def self.add_session_attributes(form, session_data)
     form.controlled = Steps::Logic.controlled?(session_data)
     form.partner = true
