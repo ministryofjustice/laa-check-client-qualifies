c668236a3ed56547dbdd34f37554223c4c96f6f4
Patrick Gleeson <patrick.gleeson@digital.justice.gov.uk>
2023-06-23 09:59:21 +0100

    Add end-2-end test

--- spec/end_to_end/self_employed_spec.rb
+++ spec/end_to_end/self_employed_spec.rb
@@ -28,11 +28,6 @@ RSpec.describe "Self-employed flow", :self_employed_flag, type: :feature do
       headers: { "Content-Type" => "application/json" },
     )
 
-    stub_request(:get, %r{state_benefit_type\z}).to_return(
-      body: [].to_json,
-      headers: { "Content-Type" => "application/json" },
-    )
-
     start_assessment
     fill_in_forms_until(:employment_status)
     fill_in_employment_status_screen(choice: "Employed or self-employed")
@@ -73,6 +68,39 @@ RSpec.describe "Self-employed flow", :self_employed_flag, type: :feature do
     fill_in_forms_until(:employment_status)
     fill_in_employment_status_screen(choice: "Employed or self-employed")
     fill_in_income_screen({ type: "Self-employment income" })
+    fill_in_forms_until(:check_answers)
+    click_on "Submit"
+
+    expect(stub).to have_been_requested
+  end
+
+  it "sends the right partner employment data to CFE" do
+    stub = stub_request(:post, %r{assessments\z}).with { |request|
+      parsed = JSON.parse(request.body)
+
+      expect(parsed["partner"]["self_employment"]).to eq(
+        [
+          {
+            "income" => {
+              "frequency" => "weekly",
+              "gross" => 1.0,
+              "national_insurance" => -0.0,
+              "tax" => -0.0,
+            },
+          },
+        ],
+      )
+    }.to_return(
+      body: FactoryBot.build(:api_result, eligible: "eligible").to_json,
+      headers: { "Content-Type" => "application/json" },
+    )
+
+    start_assessment
+    fill_in_forms_until(:applicant)
+    fill_in_applicant_screen(partner: "Yes")
+    fill_in_forms_until(:partner_employment_status)
+    fill_in_partner_employment_status_screen(choice: "Employed or self-employed")
+    fill_in_partner_income_screen({ type: "Self-employment income" })
     fill_in_forms_until(:check_answers)
     click_on "Submit"
 
--- spec/support/flow_helpers.rb
+++ spec/support/flow_helpers.rb
@@ -221,8 +221,8 @@ def fill_in_partner_employment_status_screen(choice: "Unemployed")
   fill_in_employment_status_screen(screen_name: :partner_employment_status, choice:)
 end
 
-def fill_in_partner_income_screen
-  fill_in_income_screen(screen_name: :partner_income)
+def fill_in_partner_income_screen(choices = {})
+  fill_in_income_screen(choices, screen_name: :partner_income)
 end
 
 def fill_in_partner_benefits_screen(choice: "No")
