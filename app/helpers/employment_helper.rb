0a04e596802f98606da0c59ddea95db305816cae
Patrick Gleeson <patrick.gleeson@digital.justice.gov.uk>
2023-06-28 17:30:17 +0100

    Certificated income help link

--- app/helpers/employment_helper.rb
+++ app/helpers/employment_helper.rb
@@ -9,6 +9,7 @@ module EmploymentHelper
     else
       key = "estimate_flow.#{'partner_' if partner}income.police_guidance.text"
       {
+        t("estimate_flow.income.guidance.text") => t("estimate_flow.income.guidance.certificated_link"),
         t(key) => t("estimate_flow.income.police_guidance.link"),
       }
     end
