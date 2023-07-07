beff761902170e2206653c9f46e4037ed0a979ff
willc-work <william.clarke@digital.justice.gov.uk>
2023-07-07 10:30:07 +0100

    css fix for puppetteer bug
    dont use partial for service name
    add scale for chrome print to PDF

--- app/services/pdf_service.rb
+++ app/services/pdf_service.rb
@@ -9,6 +9,10 @@ class PdfService
     },
     prefer_css_page_size: true,
     print_background: true,
+    viewport: {
+      width: 2400,
+      height: 4800,
+    },
     emulate_media: "screen",
     launch_args: ["--font-render-hinting=medium", "--no-sandbox", "--force-renderer-accessibility"],
     execute_script: "document.querySelectorAll('button').forEach(el => el.style.display = 'none')",
