5d6c4d0dc6740ac8f000188be12d7defe922cecc
Hettie Street <hettie.street@digital.justice.gov.uk>
2023-07-11 11:56:09 +0100

    [EL-978] Refactor AdditionalPropertyDetailsForm and Partner variant
    * Refactor partner and client additional property details
    * Remove BasePropertyEntryForm

--- app/forms/partner_additional_property_details_form.rb
+++ app/forms/partner_additional_property_details_form.rb
@@ -1,12 +1,15 @@
-class PartnerAdditionalPropertyDetailsForm < BasePropertyEntryForm
-  include SessionPersistableWithPrefix
+class PartnerAdditionalPropertyDetailsForm < AdditionalPropertyDetailsForm
   PREFIX = "partner_additional_".freeze
 
-  ATTRIBUTES = BASE_ATTRIBUTES
+  ATTRIBUTES = %i[house_value mortgage percentage_owned].freeze
 
   delegate :partner_additional_property_owned, to: :check
 
   def owned_with_mortgage?
     partner_additional_property_owned == "with_mortgage"
+  end
+
+  def client_form_variant?
+    false
   end
 end
