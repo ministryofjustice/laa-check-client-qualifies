9dd046c24c306b135721607a6b137d632a51ed6a
Patrick Gleeson <patrick.gleeson@digital.justice.gov.uk>
2023-07-11 16:23:22 +0100

    Stop checking CFE in readiness check

--- app/services/health_check_service.rb
+++ app/services/health_check_service.rb
@@ -1,7 +1,5 @@
 class HealthCheckService
-  def self.call(check_cfe:)
-    return false if check_cfe && !cfe_healthy?
-
+  def self.call
     database_healthy? && short_term_persistence_healthy?
   rescue StandardError
     false
@@ -16,12 +14,5 @@ class HealthCheckService
   def self.short_term_persistence_healthy?
     Rails.cache.write("_health_check_", "ok", expires_in: 5.seconds) &&
       Rails.cache.read("_health_check_") == "ok"
-  end
-
-  def self.cfe_healthy?
-    # We expect this call to return a hash with at least one key, and every value should be `true`.
-    # This shows that every part of itself that CFE considers important is currently healthy.
-    healths_collection = CfeConnection.status
-    healths_collection.keys.length.positive? && healths_collection.values.all?
   end
 end
