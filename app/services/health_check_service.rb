4825ccf73bfb005142f6b6f4af567562ee467025
Patrick Gleeson <patrick.gleeson@digital.justice.gov.uk>
2023-07-13 09:21:37 +0100

    Make health check logic more readable

--- app/services/health_check_service.rb
+++ app/services/health_check_service.rb
@@ -1,6 +1,8 @@
 class HealthCheckService
   def self.call(check_cfe:)
-    database_healthy? && short_term_persistence_healthy? && (!check_cfe || cfe_healthy?)
+    return false if check_cfe && !cfe_healthy?
+
+    database_healthy? && short_term_persistence_healthy?
   rescue StandardError
     false
   end
