f505f56e305d6a80da9e93a87848741fdf6718c4
MazOneTwoOne <76905544+MazOneTwoOne@users.noreply.github.com>
2023-07-25 17:09:28 +0100

    tweaks to hash, method and yml

--- app/services/guidance_link_service.rb
+++ app/services/guidance_link_service.rb
@@ -1,37 +1,32 @@
 class GuidanceLinkService
   class << self
-    def call(document, level_of_help, section, page_number = false )
+    def call(document:, section:)
       feature_flag = FeatureFlags.enabled?(:mtr_phase_1, without_session_data: true) ? :mtr_phase_1 : :pre_mtr_phase_1
 
-      level_of_help = level_of_help == "controlled" ? :controlled : :certificated
+      attributes[feature_flag].dig(document, section, :page_url)
 
-      attributes[feature_flag].dig(document, level_of_help, section, :page_url)
-      page_number ? attributes[feature_flag].dig(document, level_of_help, section, :page_number) : nil
+      attributes[feature_flag].dig(document, section, :page_number)
     end
 
     def attributes
       {
         pre_mtr_phase_1: {
           mental_heatlh_guidance: {
-            not_applicable: {
-              "First page": {
-                page_url: "https://assets.publishing.service.gov.uk/government/uploads/system/uploads/attachment_data/file/1143984/Mental_Health_Guidance_-_Contract_management_-_GOV.UK_2023.pdf",
-                page_number: "1",
-              },
+            "First page": {
+              page_url: "https://assets.publishing.service.gov.uk/government/uploads/system/uploads/attachment_data/file/1143984/Mental_Health_Guidance_-_Contract_management_-_GOV.UK_2023.pdf",
+              page_number: "1",
             },
           },
-          lc_guidance: { 
-            controlled: {
-              "Eligibility of children": {
-                page_url: "https://assets.publishing.service.gov.uk/government/uploads/system/uploads/attachment_data/file/1157029/Lord_Chancellor_s_guide_to_determining_financial_eligibility_for_controlled_work_and_family_mediation_May_2023.pdf#page=32",
-                page_number: "32",
-              },
+          lc_guidance_controlled: {
+            "Eligibility of children": {
+              page_url: "https://assets.publishing.service.gov.uk/government/uploads/system/uploads/attachment_data/file/1157029/Lord_Chancellor_s_guide_to_determining_financial_eligibility_for_controlled_work_and_family_mediation_May_2023.pdf#page=32",
+              page_number: "32",
             },
-            certificated: {
-              "Eligibility of children": {
-                page_url: "https://assets.publishing.service.gov.uk/government/uploads/system/uploads/attachment_data/file/1157030/Lord_Chancellor_s_guide_to_determining_financial_eligibility_for_certificated_work_May_2023__002_.pdf#page=94",
-                page_number: "94",
-              },
+          },
+          lc_guidance_certificated: {
+            "Eligibility of children": {
+              page_url: "https://assets.publishing.service.gov.uk/government/uploads/system/uploads/attachment_data/file/1157030/Lord_Chancellor_s_guide_to_determining_financial_eligibility_for_certificated_work_May_2023__002_.pdf#page=94",
+              page_number: "94",
             },
           },
           legislation: { },
