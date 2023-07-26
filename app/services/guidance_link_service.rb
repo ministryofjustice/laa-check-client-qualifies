48da5fe35887a974aec022eb88aa444a4c8d3f8a
MazOneTwoOne <76905544+MazOneTwoOne@users.noreply.github.com>
2023-07-26 11:18:29 +0100

    working prototype

--- app/services/guidance_link_service.rb
+++ app/services/guidance_link_service.rb
@@ -1,37 +1,39 @@
 class GuidanceLinkService
   class << self
-    def call(document:, section:)
+    def call(document:, section:, just_the_page_number: false)
       feature_flag = FeatureFlags.enabled?(:mtr_phase_1, without_session_data: true) ? :mtr_phase_1 : :pre_mtr_phase_1
 
-      attributes[feature_flag].dig(document, section, :page_url)
-
-      attributes[feature_flag].dig(document, section, :page_number)
+      if just_the_page_number
+        mapping[feature_flag].dig(document, section, :page_number)
+      else
+        mapping[feature_flag].dig(document, section, :page_url)
+      end
     end
 
-    def attributes
+    def mapping
       {
         pre_mtr_phase_1: {
           mental_heatlh_guidance: {
-            "First page": {
+            first_page: {
               page_url: "https://assets.publishing.service.gov.uk/government/uploads/system/uploads/attachment_data/file/1143984/Mental_Health_Guidance_-_Contract_management_-_GOV.UK_2023.pdf",
               page_number: "1",
             },
           },
           lc_guidance_controlled: {
-            "Eligibility of children": {
+            children: {
               page_url: "https://assets.publishing.service.gov.uk/government/uploads/system/uploads/attachment_data/file/1157029/Lord_Chancellor_s_guide_to_determining_financial_eligibility_for_controlled_work_and_family_mediation_May_2023.pdf#page=32",
               page_number: "32",
             },
           },
           lc_guidance_certificated: {
-            "Eligibility of children": {
+            children: {
               page_url: "https://assets.publishing.service.gov.uk/government/uploads/system/uploads/attachment_data/file/1157030/Lord_Chancellor_s_guide_to_determining_financial_eligibility_for_certificated_work_May_2023__002_.pdf#page=94",
               page_number: "94",
             },
           },
-          legislation: { },
-          controlled_work_applications: { },
-          legal_aid_learning: { },
+          legislation: {},
+          controlled_work_applications: {},
+          legal_aid_learning: {},
         },
       }
     end
