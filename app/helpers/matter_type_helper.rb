371e02b06732df706b852be914b8e4d47438da13
MazOneTwoOne <76905544+MazOneTwoOne@users.noreply.github.com>
2023-07-26 17:32:42 +0100

    stiching mapping and views - part 2

--- app/helpers/matter_type_helper.rb
+++ app/helpers/matter_type_helper.rb
@@ -1,9 +1,12 @@
 module MatterTypeHelper
   def matter_type_links
     {
-      t("estimate_flow.level_of_help.certificated_guidance.text") => t("estimate_flow.level_of_help.certificated_guidance.link"),
-      t("estimate_flow.matter_type.tribunal_guidance.text") => t("estimate_flow.matter_type.tribunal_guidance.certificated_link"),
-      t("estimate_flow.matter_type.domestic_abuse_guidance.text") => t("estimate_flow.matter_type.domestic_abuse_guidance.link"),
+      t("estimate_flow.level_of_help.certificated_guidance.text") => t("estimate_flow.level_of_help.certificated_guidance.link",
+                                                                       page_url: GuidanceLinkService.call(document: :lc_guidance_certificated, section: :first_page)),
+      t("estimate_flow.matter_type.tribunal_guidance.text") => t("estimate_flow.matter_type.tribunal_guidance.certificated_link",
+                                                                 page_url: GuidanceLinkService.call(document: :lc_guidance_certificated, section: :upper_tribunal)),
+      t("estimate_flow.matter_type.domestic_abuse_guidance.text") => t("estimate_flow.matter_type.domestic_abuse_guidance.link",
+                                                                       page_url: GuidanceLinkService.call(document: :lc_guidance_certificated, section: :domestic_abuse)),
     }
   end
 end
