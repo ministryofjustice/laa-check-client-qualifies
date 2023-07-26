46b891c745d013f7a41c29f440f56c8ec6579807
Patrick Gleeson <patrick.gleeson@digital.justice.gov.uk>
2023-07-26 11:23:33 +0100

    Add new stats for key dashboard

--- app/services/metrics_service.rb
+++ app/services/metrics_service.rb
@@ -2,7 +2,7 @@ class MetricsService
   def self.call
     return unless ENV["GECKOBOARD_ENABLED"]&.casecmp("enabled")&.zero?
 
-    Metrics::FromAnalyticsService.call
-    Metrics::FromCompletedJourneysService.call
+    Metrics::ForKeyMetricDashboardService.call
+    Metrics::ForUserJourneyDashboardService.call
   end
 end
