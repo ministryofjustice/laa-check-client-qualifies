4aa4831e26323a9973dbc3fe7483edeb91f59c4f
MazOneTwoOne <76905544+MazOneTwoOne@users.noreply.github.com>
2023-07-27 17:25:50 +0100

    stiching mapping and views - part 5

--- app/helpers/vehicle_helper.rb
+++ app/helpers/vehicle_helper.rb
@@ -1,10 +1,10 @@
 module VehicleHelper
   def vehicle_links(smod_applicable)
-    links = { t("estimate_flow.vehicle.guidance.text") => t("estimate_flow.vehicle.guidance.link"),
-              t("generic.trapped_capital.text") => t("generic.trapped_capital.certificated.link") }
+    links = { t("estimate_flow.vehicle.guidance.text") => GuidanceLinkService.call(document: :lc_guidance_certificated, chapter: :vehicle),
+              t("generic.trapped_capital.text") => GuidanceLinkService.call(document: :legal_aid_learning) }
 
     return links unless smod_applicable
 
-    links.merge({ t("generic.smod.guidance.text") => t("generic.smod.guidance.certificated.link") })
+    links.merge({ t("generic.smod.guidance.text") => GuidanceLinkService.call(document: :lc_guidance_certificated, chapter: :smod) })
   end
 end
