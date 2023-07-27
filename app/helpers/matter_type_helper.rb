f96ee4059b0cb384312324f9d442500be3b5a256
MazOneTwoOne <76905544+MazOneTwoOne@users.noreply.github.com>
2023-07-27 11:44:20 +0100

    refactor

--- app/helpers/matter_type_helper.rb
+++ app/helpers/matter_type_helper.rb
@@ -2,11 +2,11 @@ module MatterTypeHelper
   def matter_type_links
     {
       t("estimate_flow.level_of_help.certificated_guidance.text") => t("estimate_flow.level_of_help.certificated_guidance.link",
-                                                                       page_url: GuidanceLinkService.call(document: :lc_guidance_certificated, section: :first_page)),
+                                                                       page_url: GuidanceLinkService.call(document: :lc_guidance_certificated, chapter: :first_page)),
       t("estimate_flow.matter_type.tribunal_guidance.text") => t("estimate_flow.matter_type.tribunal_guidance.certificated_link",
-                                                                 page_url: GuidanceLinkService.call(document: :lc_guidance_certificated, section: :upper_tribunal)),
+                                                                 page_url: GuidanceLinkService.call(document: :lc_guidance_certificated, chapter: :upper_tribunal)),
       t("estimate_flow.matter_type.domestic_abuse_guidance.text") => t("estimate_flow.matter_type.domestic_abuse_guidance.link",
-                                                                       page_url: GuidanceLinkService.call(document: :lc_guidance_certificated, section: :domestic_abuse)),
+                                                                       page_url: GuidanceLinkService.call(document: :lc_guidance_certificated, chapter: :domestic_abuse)),
     }
   end
 end
