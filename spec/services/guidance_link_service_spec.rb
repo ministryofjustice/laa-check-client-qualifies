081932762f1105ad080dfb7712239cb0673285e2
MazOneTwoOne <76905544+MazOneTwoOne@users.noreply.github.com>
2023-07-28 14:48:36 +0100

    tests updates a LC links went live early

--- spec/services/guidance_link_service_spec.rb
+++ spec/services/guidance_link_service_spec.rb
@@ -7,22 +7,22 @@ RSpec.describe GuidanceLinkService do
 
       it "shows updated LC Guidance controlled links" do
         result = described_class.call(document: :lc_guidance_controlled)
-        expect(result).to eq "https://assets.publishing.service.gov.uk/government/uploads/system/uploads/attachment_data/file/1157029/Lord_Chancellor_s_guide_to_determining_financial_eligibility_for_controlled_work_and_family_mediation_May_2023.pdf"
+        expect(result).to eq "https://assets.publishing.service.gov.uk/government/uploads/system/uploads/attachment_data/file/1175062/Lord_Chancellor_s_guide_to_determining_financial_eligibility_for_controlled_work_and_family_mediation__July_2023_.pdf"
       end
 
       it "shows updated LC Guidance certificated links" do
         result = described_class.call(document: :lc_guidance_certificated)
-        expect(result).to eq "https://assets.publishing.service.gov.uk/government/uploads/system/uploads/attachment_data/file/1157030/Lord_Chancellor_s_guide_to_determining_financial_eligibility_for_certificated_work_May_2023__002_.pdf"
+        expect(result).to eq "https://assets.publishing.service.gov.uk/government/uploads/system/uploads/attachment_data/file/1175064/Lord_Chancellor_s_guide_to_determining_financial_eligibility_for_certificated_work__July_2023_.pdf"
       end
 
       it "shows just the page number to the correct LC Guidance, when page_number_only: is true" do
         result = described_class.call(document: :lc_guidance_controlled, sub_section: :disregarded_payments, page_number_only: true)
-        expect(result).to eq 10
+        expect(result).to eq 15
       end
 
       it "takes me to a specific part of PDF in the LC Guidance" do
         result = described_class.call(document: :lc_guidance_certificated, sub_section: :upper_tribunal)
-        expect(result).to eq "https://assets.publishing.service.gov.uk/government/uploads/system/uploads/attachment_data/file/1157030/Lord_Chancellor_s_guide_to_determining_financial_eligibility_for_certificated_work_May_2023__002_.pdf#page=127"
+        expect(result).to eq "https://assets.publishing.service.gov.uk/government/uploads/system/uploads/attachment_data/file/1175064/Lord_Chancellor_s_guide_to_determining_financial_eligibility_for_certificated_work__July_2023_.pdf#page=110"
       end
     end
 
