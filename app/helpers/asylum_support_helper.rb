b4f0f58c2478da14900f936c1ec23067bc5e4795
MazOneTwoOne <76905544+MazOneTwoOne@users.noreply.github.com>
2023-07-28 14:36:56 +0100

    updated mappping

--- app/helpers/asylum_support_helper.rb
+++ app/helpers/asylum_support_helper.rb
@@ -2,7 +2,7 @@ module AsylumSupportHelper
   def asylum_support_links(level_of_help)
     if level_of_help == "controlled"
       {
-        t("estimate_flow.asylum_support.guidance.text") => GuidanceLinkService.call(document: :lc_guidance_controlled, sub_section: :passporting_benefit),
+        t("estimate_flow.asylum_support.guidance.text") => GuidanceLinkService.call(document: :lc_guidance_controlled, sub_section: :asylum_support),
         t("estimate_flow.asylum_support.tribunal_guidance_text") => GuidanceLinkService.call(document: :lc_guidance_controlled),
       }
     else
