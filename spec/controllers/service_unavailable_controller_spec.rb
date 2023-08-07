baa2478723e671d4dd395113be81288ac9f535cb
MazOneTwoOne <76905544+MazOneTwoOne@users.noreply.github.com>
2023-08-07 12:22:05 +0100

    update of tests - still not 100% coverage :(

--- spec/controllers/service_unavailable_controller_spec.rb
+++ spec/controllers/service_unavailable_controller_spec.rb
@@ -1,10 +1,12 @@
 require "rails_helper"
 
 RSpec.describe ServiceUnavailableController, type: :controller do
-  describe "GET #index" do
-    it "has the http status of 503" do
-      get :index
-      expect(response).to have_http_status(:service_unavailable)
+  describe "#index" do
+    context "when the feature flag is turned on", :maintenance_mode do
+      it "redirects to the correct page" do
+        get :index
+        expect(response).to have_http_status(:service_unavailable)
+      end
     end
   end
 end
