d85c73f3a213a19c7645537af63528221a83e4ba
Patrick Gleeson <patrick.gleeson@digital.justice.gov.uk>
2023-08-08 09:10:42 +0100

    Add explanatory comment

--- app/services/cfe_param_builders/dependants.rb
+++ app/services/cfe_param_builders/dependants.rb
@@ -5,6 +5,10 @@ module CfeParamBuilders
         adults = details_form.adult_dependants ? Array.new(details_form.adult_dependants_count, :adult) : []
         children = details_form.child_dependants ? Array.new(details_form.child_dependants_count, :child) : []
 
+        # We tell CFE that child relatives are 17 and in full-time education. This is because
+        # the specific age of the child does not affect the base allowance, but younger children
+        # cannot have their income considered. Since we do not know the children's ages,
+        # marking them as older means that if the client does provide income, CFE will consider it.
         (adults + children).zip(dependant_incomes || []).map do |person_type, income|
           {
             date_of_birth: (person_type == :adult ? 21 : 17).years.ago.to_date,
