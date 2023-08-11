5ec1ca820d2b5df654666112a9a1fea02681d468
MazOneTwoOne <76905544+MazOneTwoOne@users.noreply.github.com>
2023-08-11 15:19:51 +0100

    check that feature flag is set-up correctly

--- spec/features/maintenance_mode_spec.rb
+++ spec/features/maintenance_mode_spec.rb
@@ -7,4 +7,11 @@ RSpec.describe "Maintenance mode page", type: :feature do
       expect(page).to have_text "Skip to main content\nGOV.UK\nCheck if your client qualifies for legal aid\nSorry, there is a problem with the service\nTry again later.\nWe have not saved your answers.\nUse our means assessment resources for guidance on checking a client's eligibility.\nContact our customer services team for support with legal aid administration or training:\ntelephone: 0300 200 2020 webchat on our training website\nAll content is available under the Open Government Licence v3.0, except where otherwise stated\n© Crown copyright"
     end
   end
+
+  context "when the flag is turned off" do
+    scenario "I can view the start page page" do
+      visit root_path
+      expect(page).to have_text "Use this service to find out if your client is likely to get civil legal aid, based on their financial situation."
+    end
+  end
 end
