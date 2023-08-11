eddb90a53a056354a16ed5709c4737b6a44489d4
MazOneTwoOne <76905544+MazOneTwoOne@users.noreply.github.com>
2023-08-11 14:52:22 +0100

    remove 'skip_before_action'

--- app/controllers/status_controller.rb
+++ app/controllers/status_controller.rb
@@ -1,6 +1,4 @@
 class StatusController < ApplicationController
-  skip_before_action :check_maintenance_mode, only: :index
-
   # This is used by both the liveness check, used to see if a pod needs replacing,
   # and the readiness check, used to see if a pod is ready to start receiving traffic
   def index
