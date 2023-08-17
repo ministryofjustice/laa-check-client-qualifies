6912d7b20a2ad84f595c67a193d0321242163661
Patrick Gleeson <patrick.gleeson@digital.justice.gov.uk>
2023-08-17 17:31:20 +0100

    Ensure updates have a sensible default published_at

--- app/models/issue_update.rb
+++ app/models/issue_update.rb
@@ -1,5 +1,5 @@
 class IssueUpdate < ApplicationRecord
   belongs_to :issue
   attribute :content, :string
-  attribute :published_at, :datetime
+  attribute :published_at, :datetime, default: -> { Time.zone.now }
 end
