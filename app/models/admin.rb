7c995d3de788558c4de210345268c8fb4136e649
Patrick Gleeson <patrick.gleeson@digital.justice.gov.uk>
2023-08-21 14:25:22 +0100

    Remove redundant bits

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,10 +1,9 @@
 class Admin < ApplicationRecord
   # This model isn't actually database authenticable, but enabling this module (and adding the blank
-  # methods below) turns on the behaviour where failed authentication checks redirect
+  # password method below) turns on the behaviour where failed authentication checks redirect
   # to a sign-in page. We use that sign-in page as a discreet place for a 'sign in with Google'
   # button
-  devise :database_authenticatable, :omniauthable, :rememberable, omniauth_providers: %i[google_oauth2]
+  devise :database_authenticatable, :omniauthable, omniauth_providers: %i[google_oauth2]
 
   def encrypted_password; end
-  def remember_created_at=(*); end
 end
