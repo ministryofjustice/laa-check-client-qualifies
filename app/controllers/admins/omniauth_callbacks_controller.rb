8eb973c982bdc6851a98ec28094ef51ff725d93b
willc-work <william.clarke@digital.justice.gov.uk>
2023-09-01 10:31:32 +0100

    test setting the url in uat

--- app/controllers/admins/omniauth_callbacks_controller.rb
+++ app/controllers/admins/omniauth_callbacks_controller.rb
@@ -3,10 +3,19 @@ module Admins
     skip_before_action :verify_authenticity_token, only: :google_oauth2
 
     def google_oauth2
+      current_url = request.url
+      puts "11111111111111"
+      puts "11111111111111"
+      puts "11111111111111"
+      puts "11111111111111"
+      puts "11111111111111"
+      puts current_url
+      binding.pry
+
       admin = Admin.find_by(email: request.env["omniauth.auth"].info.email)
 
       if admin
-        sign_in_and_redirect admin, event: :authentication
+        sign_in_and_redirect admin, event: :authentication, state: current_url
       else
         redirect_to new_admin_session_path, flash: { notice: I18n.t("devise.unrecognised") }
       end
