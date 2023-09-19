8bd68b656b54bc1ecbbff0d98dad18464756f8d1
Patrick Gleeson <113888736+patrick-laa@users.noreply.github.com>
2023-09-19 14:54:48 +0100

    EL-1165: Cope with missing params (#972)
    * Cope with missing params
    
    * Empty commit to trigger github

--- spec/controllers/documents_controller_spec.rb
+++ spec/controllers/documents_controller_spec.rb
@@ -19,5 +19,11 @@ RSpec.describe DocumentsController, type: :controller do
       expect(AnalyticsEvent.find_by(page: "other", event_type: "click_laa_cw_forms", assessment_code: "foo")).not_to be_nil
       expect(response.headers["Location"]).to eq "https://www.gov.uk/government/collections/controlled-work-application-forms"
     end
+
+    it "tracks nothing if there is no referrer param" do
+      get :show, params: { id: "lc_guidance_controlled", assessment_code: "foo" }
+      expect(AnalyticsEvent.find_by(event_type: "click_lc_guidance_controlled", assessment_code: "foo")).to be_nil
+      expect(response.headers["Location"]).to eq "https://assets.publishing.service.gov.uk/government/uploads/system/uploads/attachment_data/file/1176119/Lord_Chancellor_s_guide_to_determining_financial_eligibility_for_controlled_work_and_family_mediation__August_2023_.pdf"
+    end
   end
 end
