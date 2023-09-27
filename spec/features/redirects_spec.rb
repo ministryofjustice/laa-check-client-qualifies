bd61dc2fa08cc436c2bca9758704b267b2fee153
Patrick Gleeson <patrick.gleeson@digital.justice.gov.uk>
2023-09-27 10:53:50 +0100

    Fix redirect bug

--- spec/features/redirects_spec.rb
+++ spec/features/redirects_spec.rb
@@ -7,10 +7,16 @@ RSpec.describe "Redirects" do
     expect(page).to have_current_path("/which-matter-type/foo")
   end
 
-  scenario "I try to visit the old check answers URL for a check" do
+  scenario "I try to visit the old change answers URL for a check" do
     set_session(:foo, "level_of_help" => "controlled")
     visit "/estimates/foo/check_answers/matter_type"
     expect(page).to have_current_path("/which-matter-type/foo/check")
+  end
+
+  scenario "I try to visit the old check answers URL for a check" do
+    set_session(:foo, "level_of_help" => "controlled")
+    visit "/estimates/foo/check_answers"
+    expect(page).to have_current_path("/check-answers/foo")
   end
 
   scenario "I try to visit the old results URL for a check" do
