945efb2e3b2ce5816b0624d6b8162dc834298866
Patrick Gleeson <113888736+patrick-laa@users.noreply.github.com>
2023-09-29 16:57:40 +0100

    EL-1198: Fix timezone (#991)

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -34,7 +34,7 @@ class ResultsController < ApplicationController
 
     PdfService.with_pdf_data_from_html_string(html, request.base_url) do |pdf_data|
       send_data pdf_data,
-                filename: "#{I18n.t('generic.download_name')} - #{Time.zone.now.strftime('%Y-%m-%d %H.%M.%S')}.pdf",
+                filename: "#{I18n.t('generic.download_name')} - #{Time.zone.now.in_time_zone('London').strftime('%Y-%m-%d %H.%M.%S')}.pdf",
                 type: "application/pdf"
     end
   end
