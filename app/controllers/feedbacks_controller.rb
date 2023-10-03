8e18dc34b0644863addbf1dbe2dcc441fccb3af9
Hettie Street <hettie.street@digital.justice.gov.uk>
2023-10-03 12:19:28 +0100

    EL-479: Add controllers and routes

---
+++ app/controllers/feedbacks_controller.rb
@@ -0,0 +1,5 @@
+class FeedbacksController < ApplicationController
+  def new; end
+
+  def create; end
+end
---
+++ spec/controllers/feedbacks_controller_spec.rb
@@ -0,0 +1,11 @@
+require "rails_helper"
+
+RSpec.describe FeedbacksController, type: :controller do
+  describe "#new" do
+
+  end
+
+  describe "#create" do
+    
+  end
+end
