e90d2cfe861785b7d66b9499dfa6a86666d70bc2
MazOneTwoOne <76905544+MazOneTwoOne@users.noreply.github.com>
2023-10-04 14:08:36 +0100

    tidy up of JS & view

--- app/javascript/feedback.js
+++ app/javascript/feedback.js
@@ -11,32 +11,34 @@ const initFeedback = () => {
   document.querySelector('[data-satisfaction-feedback="yes-button"]').addEventListener('click', () => {
     linkAreaShow();
   })
+  document.querySelector('[data-satisfaction-feedback="no-button"]').addEventListener('click', () => {
+    linkAreaShow();
+  })
 };
 
-const freetextAreaShow = () => {
+function freetextAreaShow() {
   document.querySelector('[data-module="prompt-area"]').hidden = true;
   document.querySelector('[data-module="freetext-area"]').hidden = false;
   document.querySelector('[data-module="message-area"]').hidden = true;
-  document.querySelector('[data-module="freetext-feedback-area"]').style.backgroundColor = "white"
-};
+  document.querySelector('[data-module="freetext-feedback-area"]').style.backgroundColor = "white";
+}
 
-const freetextAreaHide = () => { 
+function freetextAreaHide() {
   document.querySelector('[data-module="prompt-area"]').hidden = false;
   document.querySelector('[data-module="freetext-area"]').hidden = true;
   document.querySelector('[data-module="message-area"]').hidden = true;
-  document.querySelector('[data-module="freetext-feedback-area"]').style.backgroundColor = "#f3f2f1"
-};
+  document.querySelector('[data-module="freetext-feedback-area"]').style.backgroundColor = "#f3f2f1";
+}
 
-const messageAreaShow = () => { 
+function messageAreaShow() {
   document.querySelector('[data-module="freetext-area"]').hidden = true;
   document.querySelector('[data-module="message-area"]').hidden = false;
-  document.querySelector('[data-module="freetext-feedback-area"]').style.backgroundColor = "#f3f2f1"
-};
+  document.querySelector('[data-module="freetext-feedback-area"]').style.backgroundColor = "#f3f2f1";
+}
 
-const linkAreaShow = () => { 
+function linkAreaShow() {
   document.querySelector('[data-module="question-area"]').hidden = true;
   document.querySelector('[data-module="link-area"]').hidden = false;
-};
-
+}
 
 export default initFeedback;
