abd21e5cc02901d47686e046373f63355357fc4f
MazOneTwoOne <76905544+MazOneTwoOne@users.noreply.github.com>
2023-10-04 12:33:27 +0100

    update to JS, calsses and views

--- app/javascript/feedback.js
+++ app/javascript/feedback.js
@@ -6,7 +6,10 @@ const initFeedback = () => {
     freetextAreaHide();
   })
   document.querySelector('[data-freetext-feedback="send"]').addEventListener('click', () => {
-    freetextAreaMessage();
+    messageAreaShow();
+  })
+  document.querySelector('[data-satisfaction-feedback="yes-button"]').addEventListener('click', () => {
+    linkAreaShow();
   })
 };
 
@@ -14,20 +17,25 @@ const freetextAreaShow = () => {
   document.querySelector('[data-module="prompt-area"]').hidden = true;
   document.querySelector('[data-module="freetext-area"]').hidden = false;
   document.querySelector('[data-module="message-area"]').hidden = true;
-  document.querySelector('[data-module="feedback-area"]').style.backgroundColor = "white"
+  document.querySelector('[data-module="freetext-feedback-area"]').style.backgroundColor = "white"
 };
 
 const freetextAreaHide = () => { 
   document.querySelector('[data-module="prompt-area"]').hidden = false;
   document.querySelector('[data-module="freetext-area"]').hidden = true;
   document.querySelector('[data-module="message-area"]').hidden = true;
-  document.querySelector('[data-module="feedback-area"]').style.backgroundColor = "#f3f2f1"
+  document.querySelector('[data-module="freetext-feedback-area"]').style.backgroundColor = "#f3f2f1"
 };
 
-const freetextAreaMessage = () => { 
+const messageAreaShow = () => { 
   document.querySelector('[data-module="freetext-area"]').hidden = true;
   document.querySelector('[data-module="message-area"]').hidden = false;
-  document.querySelector('[data-module="feedback-area"]').style.backgroundColor = "#f3f2f1"
+  document.querySelector('[data-module="freetext-feedback-area"]').style.backgroundColor = "#f3f2f1"
+};
+
+const linkAreaShow = () => { 
+  document.querySelector('[data-module="question-area"]').hidden = true;
+  document.querySelector('[data-module="link-area"]').hidden = false;
 };
 
 
