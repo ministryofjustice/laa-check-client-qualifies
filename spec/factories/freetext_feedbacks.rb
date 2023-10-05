d27d7de681d8977f7a77fade6c9036777b582aeb
Hettie Street <hettie.street@digital.justice.gov.uk>
2023-10-05 16:40:55 +0100

    update to tests

--- spec/factories/freetext_feedbacks.rb
+++
@@ -1,4 +0,0 @@
-FactoryBot.define do
-  factory :freetext_feedback do
-  end
-end
--- spec/system/feedback_spec.rb
+++ spec/system/feedback_spec.rb
@@ -1,27 +1,44 @@
 require "rails_helper"
 
-RSpec.describe "Feedback" do
+RSpec.describe "Feedback component" do
   let(:assessment_code) { :assessment_code }
 
   before do
     driven_by(:headless_chrome)
   end
 
-  context "when on the results page" do
-    it "I can successfully submit satisfaction feedback" do
-      # we need to stub the cfe response
-      start_assessment
-      fill_in_forms_until(:check_answers)
-      click_on "Submit"
-      expect(page).to have_content("Were you satisfied with this service?")
-      click_on "Yes"
-      expect(page).to have_content("Thank you for your feedback")
-      expect(page).to have_content("Tell us more in our 2 minute survey (opens in new tab)")
+  describe "satisafaction feedback" do
+    before do
+      stub_request(:post, %r{v6/assessments\z}).to_return(
+        body: FactoryBot.build(:api_result, eligible: "eligible").to_json,
+        headers: { "Content-Type" => "application/json" },
+      )
     end
-  end
 
-  context "when on the CW form selection page" do
-    it "I can successfully submit satisfaction feedback" do
+    context "when on the results page" do
+      it "I can successfully submit satisfaction feedback" do
+        start_assessment
+        fill_in_forms_until(:check_answers)
+        click_on "Submit"
+        expect(page).to have_content("Were you satisfied with this service?")
+        click_on "Yes"
+        expect(page).to have_content("Thank you for your feedback")
+        expect(page).to have_content("Tell us more in our 2 minute survey (opens in new tab)")
+      end
+    end
+
+    context "when on the CW form selection page" do
+      it "I can successfully submit satisfaction feedback" do
+        start_assessment
+        fill_in_level_of_help_screen(choice: "Civil controlled work or family mediation")
+        fill_in_forms_until(:check_answers)
+        click_on "Submit"
+        click_on "Continue to CW forms"
+        expect(page).to have_content("Were you satisfied with this service?")
+        click_on "No"
+        expect(page).to have_content("Thank you for your feedback")
+        expect(page).to have_content("Tell us more in our 2 minute survey (opens in new tab)")
+      end
     end
   end
 
@@ -32,35 +49,26 @@ RSpec.describe "Feedback" do
     end
   end
 
-  context "when on any other page in the flow" do
-    it "I can submit freetext feedback" do
-      visit check_answers_path(assessment_code)
-      expect(page).to have_content("Give feedback on this page")
-      click_on "Give feedback on this page"
-      fill_in "text-field", with: "some feedback!"
-      click_on "Send"
-      expect(page).to have_content("Thank you for your feedback")
-    end
+  describe "Freetext feedback" do
+    context "when on any other page in the flow" do
+      it "I can submit freetext feedback" do
+        visit check_answers_path(assessment_code)
+        expect(page).to have_content("Give feedback on this page")
+        click_on "Give feedback on this page"
+        fill_in "text-field", with: "some feedback!"
+        click_on "Send"
+        expect(page).to have_content("Thank you for your feedback")
+      end
 
-    it "I can cancel my freetext feedback" do
-      visit check_answers_path(assessment_code)
-      expect(page).to have_content("Give feedback on this page")
-      click_on "Give feedback on this page"
-      fill_in "text-field", with: "some feedback!"
-      click_on "Cancel"
-      expect(page).to have_content("Give feedback on this page")
-      expect(page).not_to have_content("Thank you for your feedback")
-    end
-  end
-
-  context "when level of help is not yet set" do
-    it "I can submit freetext feedback" do
-      visit "/what-level-help/#{assessment_code}"
-      expect(page).to have_content("Give feedback on this page")
-      click_on "Give feedback on this page"
-      fill_in "text-field", with: "some more feedback!"
-      click_on "Send"
-      expect(page).to have_content("Thank you for your feedback")
+      it "I can cancel my freetext feedback" do
+        visit check_answers_path(assessment_code)
+        expect(page).to have_content("Give feedback on this page")
+        click_on "Give feedback on this page"
+        fill_in "text-field", with: "some feedback!"
+        click_on "Cancel"
+        expect(page).to have_content("Give feedback on this page")
+        expect(page).not_to have_content("Thank you for your feedback")
+      end
     end
   end
 end
