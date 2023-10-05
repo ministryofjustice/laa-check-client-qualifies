dc5e396dc568b8246c9e24fd57a0f490d9203b90
MazOneTwoOne <76905544+MazOneTwoOne@users.noreply.github.com>
2023-10-05 15:40:12 +0100

    refactor for rubocop

--- spec/controllers/feedbacks_controller_spec.rb
+++ spec/controllers/feedbacks_controller_spec.rb
@@ -2,15 +2,17 @@ require "rails_helper"
 
 RSpec.describe FeedbacksController, type: :controller do
   describe "#create" do
+    before do
+      allow(controller).to receive(:level_of_help).and_return("controlled")
+    end
+
     it "saves freetext feedback correctly" do
-      allow_any_instance_of(described_class).to receive(:level_of_help).and_return("controlled")
       post :create, params: { type: "freetext", text: "foo", page: "bar" }
       expect(FreetextFeedback.find_by(text: "foo", page: "bar", level_of_help: "controlled")).not_to be_nil
     end
 
     it "saves satisfaction feedback correctly" do
-      allow_any_instance_of(described_class).to receive(:level_of_help).and_return("controlled")
-      allow_any_instance_of(described_class).to receive(:outcome).and_return("eligible")
+      allow(controller).to receive(:outcome).and_return("eligible")
       post :create, params: { type: "satisfaction", satisfied: "yes" }
       expect(SatisfactionFeedback.find_by(satisfied: "yes", level_of_help: "controlled", outcome: "eligible")).not_to be_nil
     end
