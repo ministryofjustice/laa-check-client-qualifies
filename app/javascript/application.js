1a7b9eff5367266c348bf2c96582616881c706e0
Patrick Gleeson <patrick.gleeson@digital.justice.gov.uk>
2023-10-12 15:34:22 +0100

    Adjust to get Sentry configured before anything else

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,7 +1,19 @@
+import * as Sentry from "@sentry/browser";
+
+const sentryDsn = document.querySelector("body").dataset.sentryDsn;
+if (sentryDsn) {
+  Sentry.init({
+    dsn: sentryDsn,
+    integrations: [new Sentry.BrowserTracing(), new Sentry.Replay()],
+    tracesSampleRate: 1.0,
+    replaysSessionSampleRate: 0.1,
+    replaysOnErrorSampleRate: 1.0,
+  });
+}
+
 import { initAll } from "govuk-frontend";
 import initResults from "./results";
 import Rails from '@rails/ujs';
-import * as Sentry from "@sentry/browser";
 
 // NOTE: suggestions input component not yet part of GOV.UK frontend
 // https://github.com/alphagov/govuk-frontend/pull/2453
@@ -29,13 +41,4 @@ initAll();
 initResults();
 initFeedback();
 
-const sentryDsn = document.querySelector("body").dataset.sentryDsn;
-if (sentryDsn) {
-  Sentry.init({
-    dsn: sentryDsn,
-    integrations: [new Sentry.BrowserTracing(), new Sentry.Replay()],
-    tracesSampleRate: 1.0,
-    replaysSessionSampleRate: 0.1,
-    replaysOnErrorSampleRate: 1.0,
-  });
-}
+
