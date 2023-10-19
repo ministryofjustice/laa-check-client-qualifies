08b8dce060bffa9d1e92288a50a77ec4a8777e82
Patrick Gleeson <113888736+patrick-laa@users.noreply.github.com>
2023-10-19 10:44:17 +0100

    EL-1208: 404 instead of 500 on typo (#1016)

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -14,8 +14,11 @@ Rails.application.configure do
     # This is the SHA for the inline javascript used by the static service unavailable page (as it's static, it can't use a nonce)
     policy.script_src  "'sha256-+6WnXIl4mbFTCARd8N3COQmT3bJJmo32N8q8ZSQAIcU='", :self, :https, :report_sample
 
-    # This is the SHA for the inline CSS used by the static service unavailable page (as it's static, it can't use a nonce)
-    policy.style_src   "'sha256-77rnAjTWIfn0C+JGy/VQ5ZlAh2ZNEkgbAZfHP+ot9k0='", :self, :https, :report_sample
+    # These are the SHAs for the inline CSS used by the static service unavailable and not found pages
+    # (as they're static, they can't use a nonce)
+    policy.style_src "'sha256-77rnAjTWIfn0C+JGy/VQ5ZlAh2ZNEkgbAZfHP+ot9k0='",
+                     "'sha256-SKk0A/cx4rPskCI+pT65OEYkqpmpNRbLvwc1gsUtf6s='",
+                     :self, :https, :report_sample
 
     # Specify URI for violation reports
     policy.report_uri(ENV["CSP_REPORT_ENDPOINT"]) if ENV["CSP_REPORT_ENDPOINT"].present?
