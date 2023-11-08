71d361449ff658783009b050df7dca8aacefadf1
MazOneTwoOne <76905544+MazOneTwoOne@users.noreply.github.com>
2023-11-08 14:59:40 +0000

    update

--- app/lib/feature_flags.rb
+++ app/lib/feature_flags.rb
@@ -13,7 +13,7 @@ class FeatureFlags
     maintenance_mode: "global",
     basic_authentication: "global",
     welsh_cw: "global",
-    end_of_journey: "global",
+    end_of_journey: "session",
   }.freeze
 
   class << self
