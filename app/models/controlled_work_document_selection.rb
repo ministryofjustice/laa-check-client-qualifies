3ac8584628bcc85b6c36bb71ecacdc3db95b3d84
MazOneTwoOne <76905544+MazOneTwoOne@users.noreply.github.com>
2023-11-22 14:52:19 +0000

    WIP

--- app/models/controlled_work_document_selection.rb
+++ app/models/controlled_work_document_selection.rb
@@ -6,12 +6,31 @@ class ControlledWorkDocumentSelection
   ATTRIBUTES = %i[form_type language].freeze
 
   OPTIONS = %i[cw1 cw2 cw1_and_2 cw5 civ_means_7].freeze
+  OPTIONS_UNDER18_CLR = %i[cw2 cw1_and_2].freeze
   LANGUAGES = %w[english welsh].freeze
 
   attribute :form_type, :string
-  validates :form_type, presence: true, inclusion: { in: OPTIONS.map(&:to_s), allow_nil: true }
+  validates :form_type, presence: true, inclusion: { in: ->(options) { options.form_options }, allow_nil: true }
 
   attribute :language, :string
   validates :language, presence: true, inclusion: { in: LANGUAGES, allow_nil: true },
                        if: -> { FeatureFlags.enabled?(:welsh_cw, without_session_data: true) }
+
+  # Based on the IncomeModel class
+  # Will need to chanege :level_of_help to whatever the CLR option is
+  attr_accessor :level_of_help, :partner
+
+  # My thinking is that this checks if feature flag is on and then level of help is true
+  # Will need to chanege :level_of_help to whatever the CLR option is
+  def client_under_18_and_clr
+    FeatureFlags.enabled?(:under_eighteen, @check.session_data) && partner
+  end
+
+  def form_options
+    if client_under_18_and_clr
+      OPTIONS_UNDER18_CLR.map(&:to_s)
+    else
+      OPTIONS.map(&:to_s)
+    end
+  end
 end
