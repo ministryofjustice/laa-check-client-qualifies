8ef7d566e3fbde5352c48cc889c6d991401bf06d
MazOneTwoOne <76905544+MazOneTwoOne@users.noreply.github.com>
2023-11-29 16:50:15 +0000

    EL-1277: Under 18s - CLR - Select CW form - 2nd Attempt (#1137)
    * Interim commit
    
    * Add content for CLR question
    
    * Adjust spec to include CLR
    
    * filtering cw form radio options
    
    * update to spec
    
    * update from review comments
    
    ---------
    
    Co-authored-by: Patrick Gleeson <patrick.gleeson@digital.justice.gov.uk>

--- app/models/controlled_work_document_selection.rb
+++ app/models/controlled_work_document_selection.rb
@@ -16,7 +16,14 @@ class ControlledWorkDocumentSelection
                        if: -> { FeatureFlags.enabled?(:welsh_cw, without_session_data: true) }
 
   def options
-    OPTIONS.select { !check.asylum_support || %i[cw1 cw2].include?(_1) }
-           .map { { value: _1, options: { label: { text: I18n.t("controlled_work_document_selections.new.option.#{_1}") } } } }
+    cw_form_radio_button_option = if check.controlled_legal_representation
+                                    %i[cw2 cw1_and_2].freeze
+                                  elsif check.asylum_support
+                                    %i[cw1 cw2].freeze
+                                  else
+                                    OPTIONS
+                                  end
+
+    cw_form_radio_button_option.map { { value: _1, options: { label: { text: I18n.t("controlled_work_document_selections.new.option.#{_1}") } } } }
   end
 end
