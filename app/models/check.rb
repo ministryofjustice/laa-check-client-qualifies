4b68598d5f27ce1c0611910515508e15efa1282a
MazOneTwoOne <76905544+MazOneTwoOne@users.noreply.github.com>
2023-11-29 12:21:39 +0000

    update from review comments

--- app/models/check.rb
+++ app/models/check.rb
@@ -114,10 +114,6 @@ class Check
     end
   end
 
-  def under_eighteen_controlled_clr?
-    Steps::Logic.controlled_clr?(session_data)
-  end
-
   def under_eighteen_no_means_test_required?
     Steps::Logic.under_eighteen_no_means_test_required?(session_data)
   end
