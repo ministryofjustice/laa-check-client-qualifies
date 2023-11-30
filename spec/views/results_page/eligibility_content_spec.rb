04e4fd863089c7ce1c63b5fa9bf2fbb897178985
Patrick Gleeson <113888736+patrick-laa@users.noreply.github.com>
2023-11-30 11:11:13 +0000

    EL-1111: Certificated/ineligible results text (#1143)
    * Adjust content for ineligible certificated results
    
    * UAT comment

--- spec/views/results_page/eligibility_content_spec.rb
+++ spec/views/results_page/eligibility_content_spec.rb
@@ -25,6 +25,7 @@ RSpec.describe "results/show.html.slim" do
 
       it "show eligible" do
         expect(page_text).to include "Your client is likely to qualify for civil legal aid"
+        expect(page_text).to include "Use Apply for legal aid or CCMS to start an application for your client."
       end
     end
 
@@ -83,7 +84,8 @@ RSpec.describe "results/show.html.slim" do
         expect(page_text).to include(
           "Your client’s total monthly income exceeds the upper limit. This means they do not qualify for legal aid.",
         )
-        expect(page_text).to include("Your client is not likely to qualify for civil legal aid")
+        expect(page_text).to include("Your client is not likely to qualify for civil legal aid, for certificated work")
+        expect(page_text).to include("You may wish to discuss your client’s options")
       end
     end
   end
