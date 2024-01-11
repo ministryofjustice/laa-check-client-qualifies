c221c59d8f92abbccd4da7a10a78ed21d7d2d024
willc-work <william.clarke@digital.justice.gov.uk>
2024-01-11 13:09:16 +0000

    WIP tests

--- spec/flows/early_result_certificated_non_passported_spec.rb
+++ spec/flows/early_result_certificated_non_passported_spec.rb
@@ -28,9 +28,10 @@ RSpec.describe "Early Result Certificated, non-passported flow", type: :feature 
   end
 
   it "when I am ineligible on gross income and continue the check" do
-    stub = stub_request(:post, %r{assessments\z}).with do |request|
-      expect(request.headers["User-Agent"]).to match(/ccq\/.* \(.*\)/)
-    end
+    # stub = stub_request(:post, %r{assessments\z}).with do |request|
+    #   expect(request.headers["User-Agent"]).to match(/ccq\/.* \(.*\)/)
+    # end
+    stub = allow(CfeService).to receive(:call).and_return(api_response)
 
     start_assessment
     fill_in_forms_until(:applicant)
@@ -40,10 +41,10 @@ RSpec.describe "Early Result Certificated, non-passported flow", type: :feature 
     fill_in_income_screen({ gross: "8000", frequency: "Every month" })
     fill_in_forms_until(:other_income)
     fill_in_other_income_screen(values: { friends_or_family: "1200" }, frequencies: { friends_or_family: "Every week" })
-    # allow(CfeService).to receive(:call).and_return build(:early_gross_income_result)
-    allow(CfeService).to receive(:call).and_return :api_response
+    expect(stub).to have_received(:call).with(session_data, :gross_income)
+    # allow(CfeService).to receive(:call).and_return(api_response)
     # confirm_screen("ineligible-gross-income")
-    expect(stub).to have_been_requested
+    # expect(stub).to have_been_requested
     bypass_early_gross_income_result("Return to check")
     confirm_screen("Outgoings")
     fill_in_outgoings_screen
