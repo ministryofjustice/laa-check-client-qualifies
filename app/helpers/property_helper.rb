f69d011c83cbe03f7c5b7513d3bf8755f11077dc
MazOneTwoOne <76905544+MazOneTwoOne@users.noreply.github.com>
2024-02-19 15:24:36 +0000

    updaate to tranlation file and helpers

--- app/helpers/property_helper.rb
+++ app/helpers/property_helper.rb
@@ -3,15 +3,15 @@ module PropertyHelper
     basic = non_smod_property_links(level_of_help, additional_property)
     return basic unless smod_applicable
 
-    basic.merge({ t("generic.smod.guidance.text") => document_link(:"lc_guidance_#{level_of_help}", :smod) })
+    basic.merge({ t("generic.smod.guidance.#{level_of_help}_text") => document_link(:"lc_guidance_#{level_of_help}", :smod) })
   end
 
   def non_smod_property_links(level_of_help, additional_property)
     guidance_type = additional_property ? "additional_properties_guidance" : "properties_guidance"
 
     {
-      t("question_flow.property.#{guidance_type}.text") => document_link(:"lc_guidance_#{level_of_help}", :"#{guidance_type}"),
-      t("generic.trapped_capital.text") => (level_of_help == "controlled" ? document_link(:lc_guidance_controlled, :assets) : document_link(:legal_aid_learning)),
+      t("question_flow.property.#{guidance_type}.#{level_of_help}.text") => document_link(:"lc_guidance_#{level_of_help}", :"#{guidance_type}"),
+      t("generic.trapped_capital.#{level_of_help}_text") => (level_of_help == "controlled" ? document_link(:lc_guidance_controlled, :assets) : document_link(:legal_aid_learning)),
     }
   end
 end
