33aa50a81a59179be1a03ef00da9931a02d3fc36
MazOneTwoOne <76905544+MazOneTwoOne@users.noreply.github.com>
2024-02-21 17:55:50 +0000

    updates

--- app/helpers/asylum_support_helper.rb
+++ app/helpers/asylum_support_helper.rb
@@ -2,12 +2,12 @@ module AsylumSupportHelper
   def asylum_support_links(level_of_help)
     if level_of_help == "controlled"
       {
-        t("question_flow.asylum_support.guidance.controlled_text") => document_link(:lc_guidance_controlled, :asylum_support),
-        t("question_flow.asylum_support.tribunal_guidance_text") => document_link(:lc_guidance_controlled),
+        t("question_flow.asylum_support.guidance.text") => { document: document_link(:lc_guidance_controlled, :asylum_support), file_info: file_info(:lc_guidance_controlled) },
+        t("question_flow.asylum_support.tribunal_guidance_text") => { document: document_link(:lc_guidance_controlled), file_info: file_info(:lc_guidance_controlled) },
       }
     else
       {
-        t("question_flow.asylum_support.guidance.certificated_text") => document_link(:lc_guidance_certificated, :upper_tribunal),
+        t("question_flow.asylum_support.guidance.text") => { document: document_link(:lc_guidance_certificated, :upper_tribunal), file_info: file_info(:lc_guidance_certificated) },
       }
     end
   end
--- app/helpers/under_18_clr_helper.rb
+++ app/helpers/under_18_clr_helper.rb
@@ -1,9 +1,9 @@
 module Under18ClrHelper
   def under_18_clr_links
     {
-      t("question_flow.under_18_clr.guidance.controlled") => document_link("lc_guidance_controlled"),
-      t("question_flow.under_18_clr.guidance.mental_health") => document_link("mental_health_guidance"),
-      t("question_flow.under_18_clr.guidance.clr") => document_link("lc_guidance_controlled", :under_18),
+      t("question_flow.under_18_clr.guidance.controlled") => { document: document_link(:lc_guidance_controlled), file_info: file_info(:lc_guidance_controlled) },
+      t("question_flow.under_18_clr.guidance.mental_health") => { document: document_link(:mental_health_guidance), file_info: file_info(:mental_health_guidance) },
+      t("question_flow.under_18_clr.guidance.clr") => { document: document_link(:lc_guidance_controlled, :under_18), file_info: file_info(:lc_guidance_controlled) },
     }
   end
 end
