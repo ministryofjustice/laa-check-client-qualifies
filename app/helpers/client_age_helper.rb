44d3a5c96c618703af3c3eff572aaa7a022c6871
MazOneTwoOne <76905544+MazOneTwoOne@users.noreply.github.com>
2024-02-21 16:43:47 +0000

    Part 2 of PDF link service, having PDF names in the one place

--- app/helpers/client_age_helper.rb
+++ app/helpers/client_age_helper.rb
@@ -1,12 +1,12 @@
 module ClientAgeHelper
   def client_age_links
     {
-      t("question_flow.client_age.guidance.controlled") => document_link("lc_guidance_controlled"),
-      t("question_flow.client_age.guidance.certificated") => document_link("lc_guidance_certificated"),
-      t("question_flow.client_age.guidance.under_18_controlled") => document_link("lc_guidance_controlled", :under_18),
-      t("question_flow.client_age.guidance.under_18_certificated") => document_link("lc_guidance_certificated", :under_18),
-      t("question_flow.client_age.guidance.over_60_controlled") => document_link("lc_guidance_controlled", :over_60),
-      t("question_flow.client_age.guidance.over_60_certificated") => document_link("lc_guidance_certificated", :over_60),
+      t("question_flow.client_age.guidance.controlled") => { document: document_link("lc_guidance_controlled"), file_info: file_info(:lc_guidance_controlled) },
+      t("question_flow.client_age.guidance.certificated") => { document: document_link("lc_guidance_certificated"), file_info: file_info(:lc_guidance_certificated) },
+      t("question_flow.client_age.guidance.under_18_controlled") => { document: document_link("lc_guidance_controlled", :under_18), file_info: file_info(:lc_guidance_controlled) },
+      t("question_flow.client_age.guidance.under_18_certificated") => { document: document_link("lc_guidance_certificated", :under_18), file_info: file_info(:lc_guidance_certificated) },
+      t("question_flow.client_age.guidance.over_60_controlled") => { document: document_link("lc_guidance_controlled", :over_60), file_info: file_info(:lc_guidance_controlled) },
+      t("question_flow.client_age.guidance.over_60_certificated") => { document: document_link("lc_guidance_certificated", :over_60), file_info: file_info(:lc_guidance_certificated) },
     }
   end
 end
