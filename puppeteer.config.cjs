c00ccfc9ae0a00d94c808fb290fcda51a06b3bd0
MazOneTwoOne <76905544+MazOneTwoOne@users.noreply.github.com>
2024-02-28 15:08:45 +0000

    add the configuration file back in to check this is affecting the tests

---
+++ puppeteer.config.cjs
@@ -0,0 +1,11 @@
+// found this from the guidance: https://pptr.dev/guides/configuration
+
+const {join} = require('path');
+
+/**
+ * @type {import("puppeteer").Configuration}
+ */
+module.exports = {
+  // Changes the cache location for Puppeteer.
+  cacheDirectory: join(__dirname, '.cache', 'puppeteer'),
+};
