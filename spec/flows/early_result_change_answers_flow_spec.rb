49c2abe27b2b7c859567e1b5751ddd5125a316c3
Stephen Dicks <github@starswan.com>
2024-03-05 08:06:05 +0000

    Add test and fix for switching from passporting to not (and making ineligible on gross)

--- spec/flows/early_result_change_answers_flow_spec.rb
+++ spec/flows/early_result_change_answers_flow_spec.rb
@@ -233,26 +233,6 @@ RSpec.describe "Change answers after early result", :early_eligibility_flag, typ
       expect(page).to have_content "What is your client's employment status?Unemployed"
     end
 
-    it "can handle a switch from passporting to not" do
-      start_assessment
-      fill_in_forms_until(:applicant)
-      fill_in_applicant_screen(passporting: "Yes")
-      fill_in_forms_until(:check_answers)
-      within "#table-applicant" do
-        click_on "Change"
-      end
-      fill_in_applicant_screen(passporting: "No")
-      expect(page).not_to have_selector(".govuk-notification-banner")
-      fill_in_dependant_details_screen
-      expect(page).not_to have_selector(".govuk-notification-banner")
-      fill_in_employment_status_screen
-      fill_in_benefits_screen
-      fill_in_other_income_screen
-      fill_in_outgoings_screen
-      fill_in_housing_costs_screen
-      confirm_screen("check_answers")
-    end
-
     it "takes me on mini loops" do
       start_assessment
       fill_in_forms_until(:vehicle)
@@ -293,5 +273,29 @@ RSpec.describe "Change answers after early result", :early_eligibility_flag, typ
       fill_in_immigration_or_asylum_screen
       confirm_screen("check_answers")
     end
+  end
+
+  it "can handle a switch from passporting to not", :vcr do
+    start_assessment
+    fill_in_forms_until(:applicant)
+    fill_in_applicant_screen(passporting: "Yes")
+    fill_in_forms_until(:check_answers)
+    within "#table-applicant" do
+      click_on "Change"
+    end
+    fill_in_applicant_screen(passporting: "No")
+    expect(page).not_to have_selector(".govuk-notification-banner")
+    fill_in_dependant_details_screen
+    expect(page).not_to have_selector(".govuk-notification-banner")
+    fill_in_employment_status_screen(choice: "Employed")
+    fill_in_income_screen(gross: "2700")
+    fill_in_benefits_screen
+    fill_in_other_income_screen
+    fill_in_outgoings_screen
+    fill_in_housing_costs_screen
+    confirm_screen("check_answers")
+    click_on 'Submit'
+    # check that result isn't 'your answers have been deleted'
+    expect(page).to have_content "Your client's key eligibility totals"
   end
 end
