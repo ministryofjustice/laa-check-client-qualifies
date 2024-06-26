92136853b582c4084ba40845f8fabe115c013621
Stephen Dicks <github@starswan.com>
2024-06-26 09:41:24 +0100

    put back migration

--- db/migrate/20230426104843_add_blazer_permissions.rb
+++ db/migrate/20230426104843_add_blazer_permissions.rb
@@ -1,18 +1,15 @@
 class AddBlazerPermissions < ActiveRecord::Migration[7.0]
   def up
-    # Note this will never to true in non-UAT environments
-    # without it UAT migrations can't run properly once blazer password has been removed
-    # as they run all migrations on startup
-    password = ENV.fetch("BLAZER_DATABASE_PASSWORD", "password")
+    return if ENV["BLAZER_DATABASE_PASSWORD"].blank?
 
     ApplicationRecord.connection.execute(
       "BEGIN;
-      CREATE ROLE blazer LOGIN PASSWORD '#{password}';
+      CREATE ROLE blazer LOGIN PASSWORD '#{ENV['BLAZER_DATABASE_PASSWORD']}';
       GRANT CONNECT ON DATABASE #{ENV['POSTGRES_DATABASE']} TO blazer;
       GRANT USAGE ON SCHEMA public TO blazer;
       GRANT SELECT ON ALL TABLES IN SCHEMA public TO blazer;
       ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT SELECT ON TABLES TO blazer;
       COMMIT;",
-    )
+      )
   end
 end
