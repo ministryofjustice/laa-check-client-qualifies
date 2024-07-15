323a26878c367dec1811282f481845ed4d9538b3
Stephen Dicks <github@starswan.com>
2024-07-15 15:33:57 +0100

    remove commented out code

--- spec/factories/mock_saml_auth.rb
+++ spec/factories/mock_saml_auth.rb
@@ -1,14 +1,4 @@
 FactoryBot.define do
-  # OmniAuth::AuthHash.new(
-  #   provider: "saml",
-  #   uid: "test-user",
-  #   info: {
-  #     email: "provider@example.com",
-  #     roles: %w[CCQ],
-  #     office_codes: %w[1A123B 2A555X 3B345C 4C567D],
-  #   },
-  #   )
-
   factory :mock_saml_auth, class: OmniAuth::AuthHash do
     provider { "saml" }
     uid { "test-user" }
