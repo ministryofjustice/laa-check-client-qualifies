6aaac6f1f7bbf95d75bafe119d897c72c87e21d4
Stephen Dicks <github@starswan.com>
2024-07-22 08:02:03 +0100

    EL-1612 delete providers created over a week ago

--- app/services/old_data_removal_service.rb
+++ app/services/old_data_removal_service.rb
@@ -3,5 +3,11 @@ class OldDataRemovalService
     cutoff = 5.years.ago
     AnalyticsEvent.where("created_at < ?", cutoff).delete_all
     CompletedUserJourney.where("completed < ?", cutoff).delete_all
+
+    # storing Provider from the Portal is a bit annoying -
+    # so for data protection purposes we are just deleting them
+    # once a week. This might have to stop once this is used as a foreign
+    # key into other tables e.g. saved checks but for now this is fine
+    Provider.where("created_at < ?", 1.week.ago).delete_all
   end
 end
