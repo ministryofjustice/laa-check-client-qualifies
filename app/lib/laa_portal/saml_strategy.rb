c9071877774a72c9f58930635d9823a4281ca447
Stephen Dicks <github@starswan.com>
2024-07-30 16:48:52 +0100

    EL-1660 Add error logging for SAML Logout request error

--- app/lib/laa_portal/saml_strategy.rb
+++ app/lib/laa_portal/saml_strategy.rb
@@ -18,6 +18,32 @@ module LaaPortal
         Rails.logger.debug("auth_hash after #{auth.info}")
       end
     end
+
+    # temp - copy of gem code with logging added
+    def handle_logout_request(raw_request, settings)
+      logout_request = OneLogin::RubySaml::SloLogoutrequest.new(raw_request, {}.merge(settings: settings).merge(get_params: @request.params))
+
+      valid_logout_request = logout_request.is_valid?(true)
+      saml_uid = session["saml_uid"]
+      if valid_logout_request &&
+        logout_request.name_id == saml_uid
+
+        # Actually log out this session
+        options[:idp_slo_session_destroy].call @env, session
+
+        # Generate a response to the IdP.
+        logout_request_id = logout_request.id
+        logout_response = OneLogin::RubySaml::SloLogoutresponse.new.create(settings, logout_request_id, nil, RelayState: slo_relay_state)
+        redirect(logout_response)
+      else
+        if valid_logout_request
+          raise OmniAuth::Strategies::SAML::ValidationError.new("SAMl Logout request failed: Name id #{logout_request.name_id} SAML_UID #{saml_uid}")
+        else
+          raise OmniAuth::Strategies::SAML::ValidationError.new("SAML Logout request failed: Errors #{logout_request.errors.inspect}")
+        end
+      end
+    end
+
     # :nocov:
   end
 end
