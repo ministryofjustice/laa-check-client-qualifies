7fb1429f13171eb1a635a413e4a19c704fda818c
MazOneTwoOne <76905544+MazOneTwoOne@users.noreply.github.com>
2024-08-01 13:48:33 +0100

    - update from review comments

--- spec/features/instant_sessions_spec.rb
+++ spec/features/instant_sessions_spec.rb
@@ -9,33 +9,6 @@ RSpec.describe "Instant sessions page" do
     allow(CfeConnection).to receive(:assess).and_return(api_response)
   end
 
-  context "without conditional reveals for assets enabled" do
-    scenario "I request an instant controlled session" do
-      visit "instant-controlled"
-      expect(page).to have_content "Check your answers"
-      expect(page).to have_content "Income before any deductions£1.00"
-      expect(page).to have_content "Does your client get financial help from friends or family?\nNo"
-      click_on "Submit"
-      expect(page).to have_content "Your client qualifies financially for civil legal aid"
-    end
-
-    scenario "I request a content-rich instant controlled session" do
-      visit "instant-controlled-scenario"
-      expect(page).to have_content "Income before any deductions£1,200.00"
-      click_on "Submit"
-      expect(page).to have_content "Your client qualifies financially for civil legal aid"
-    end
-
-    scenario "I request an instant certificated session" do
-      visit "instant-certificated"
-      expect(page).to have_content "Check your answers"
-      expect(page).to have_content "Income before any deductions£1.00"
-      expect(page).to have_content "Does your client get financial help from friends or family?\nNo"
-      click_on "Submit"
-      expect(page).to have_content "Your client is likely to qualify financially for civil legal aid"
-    end
-  end
-
   scenario "I request an instant controlled session" do
     visit "instant-controlled"
     expect(page).to have_content "Check your answers"
