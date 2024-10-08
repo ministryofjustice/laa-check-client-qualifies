c676bbe2e8cc85fda3edb90eb3bc7d6e341db956
willclarke <william.clarke@digital.justice.gov.uk>
2024-10-08 10:32:25 +0100

    remove hexapdf (#1565)

--- app/services/pdf_service.rb
+++ app/services/pdf_service.rb
@@ -19,24 +19,9 @@ class PdfService
 
   class << self
     def with_pdf_data_from_html_string(html_string, display_url)
-      Tempfile.open("initial_pdf") do |initial_pdf|
-        pdf_data = Grover.new(html_string, **GROVER_OPTIONS.merge(display_url:)).to_pdf
-        initial_pdf.write(pdf_data.force_encoding("UTF-8"))
-        initial_pdf.rewind
-
-        # Grover doesn't know how to set language metadata in the PDF 'Catalog'
-        # (see https://github.com/Studiosity/grover/issues/190#issuecomment-1517122470)
-        # so we do it manually here
-        editable = HexaPDF::Document.open(initial_pdf.path)
-        editable.catalog.value[:Lang] = "en-GB"
-
-        Tempfile.open("modified_pdf") do |modified_pdf|
-          editable.write(modified_pdf.path)
-          modified_pdf.rewind
-
-          yield modified_pdf.read
-        end
-      end
+      pdf_data = Grover.new(html_string, **GROVER_OPTIONS.merge(display_url:)).to_pdf
+      pdf_data.force_encoding("UTF-8")
+      yield pdf_data
     end
   end
 end
