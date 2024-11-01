d79c76850955b09e4141d45f6209cdf823be786b
Will Clarke <william.clarke@digital.justice.gov.uk>
2024-11-01 12:04:14 +0000

    add new column

---
+++ db/migrate/20241101120059_populate_early_eligibility_result_in_completed_user_journeys.rb
@@ -0,0 +1,5 @@
+class PopulateEarlyEligibilityResultInCompletedUserJourneys < ActiveRecord::Migration[7.2]
+  def change
+    add_column :completed_user_journeys, :early_eligibility_result, :boolean
+  end
+end
