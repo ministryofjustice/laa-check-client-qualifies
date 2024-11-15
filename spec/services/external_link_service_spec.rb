f2e177b8d0b0849a542e4a2e323e07a554e2f016
MazOneTwoOne <76905544+MazOneTwoOne@users.noreply.github.com>
2024-11-15 15:27:46 +0000

    - add in missing partner translations

--- spec/services/external_link_service_spec.rb
+++ spec/services/external_link_service_spec.rb
@@ -1,6 +1,6 @@
 require "rails_helper"
 
-RSpec.describe ExternalLinkService do
+RSpec.describe ExternalLinkService, :mtr_accelerated do
   describe ".call" do
     it "shows controlled LC Guidance links" do
       result = described_class.call(document: :lc_guidance_controlled)
@@ -19,7 +19,7 @@ RSpec.describe ExternalLinkService do
 
     it "takes me to a specific part of PDF in the LC Guidance" do
       result = described_class.call(document: :lc_guidance_certificated, sub_section: :upper_tribunal)
-      expect(result).to eq "https://assets.publishing.service.gov.uk/government/uploads/system/uploads/attachment_data/file/1176073/Lord_Chancellor_s_guide_to_determining_financial_eligibility_for_certificated_work__August_2023_.pdf#page=110"
+      expect(result).to eq "https://assets.publishing.service.gov.uk/government/uploads/system/uploads/attachment_data/file/1176073/Lord_Chancellor_s_guide_to_determining_financial_eligibility_for_certificated_work__August_2023_.pdf#page=125"
     end
 
     it "takes me to the external CW form page" do
