b22ba5859cd7bbe192046fa6ffcf4c0377bd89fc
MazOneTwoOne <76905544+MazOneTwoOne@users.noreply.github.com>
2024-12-19 16:51:19 +0000

    - add new pdfs - rename mappings - update tests - remove redundant feature flag

--- app/services/controlled_work_document_population_service.rb
+++ app/services/controlled_work_document_population_service.rb
@@ -10,10 +10,7 @@ class ControlledWorkDocumentPopulationService
   class << self
     def call(session_data, model)
       Dir.mktmpdir do |dir|
-        # Commenting this out until all the new CW forms are updated then this will be reinstated
-        # form_key = "#{model.form_type}#{'_welsh' if model.language == 'welsh'}"
-        # We will use the method below to add _new suffix
-        form_key = generate_form_key(model)
+        form_key = "#{model.form_type}#{'_welsh' if model.language == 'welsh'}"
         file_name = "#{dir}/output-form.pdf"
         pdftk = PdfForms.new(`which pdftk`.chomp)
         pdftk.fill_form template_path(form_key), file_name, values(session_data, form_key)
@@ -22,20 +19,16 @@ class ControlledWorkDocumentPopulationService
     end
 
     TEMPLATES = {
-      "cw1" => "lib/cw1-form.pdf",
-      "cw1_new" => "lib/cw1-form-new.pdf",
-      "cw2" => "lib/cw2imm-form-2023-8-21.pdf",
+      "cw1" => "lib/cw1-form-CCQoct24.pdf",
+      "cw2" => "lib/cw2imm-form-CCQoct24.pdf",
       "cw5" => "lib/cw5-form.pdf",
-      "cw1_and_2" => "lib/cw1-and-2-form-2024-09-20.pdf",
-      "civ_means_7" => "lib/civ-means-7-form.pdf",
-      "civ_means_7_new" => "lib/civ-means-7-form-new.pdf",
-      "cw1_welsh" => "lib/cw1-form-welsh.pdf",
-      "cw1_welsh_new" => "lib/cw1-form-welsh-new.pdf",
-      "cw2_welsh" => "lib/cw2imm-form-welsh.pdf",
-      "cw5_welsh" => "lib/cw5-form-welsh.pdf",
-      "cw1_and_2_welsh" => "lib/cw1-and-2-form-welsh.pdf",
-      "civ_means_7_welsh" => "lib/civ-means-7-form-welsh.pdf",
-      "civ_means_7_welsh_new" => "lib/civ-means-7-form-welsh-new.pdf",
+      "cw1_and_2" => "lib/cw1-and-2-form-CCQoct24.pdf",
+      "civ_means_7" => "lib/civ-means-7-form-CCQoct24.pdf",
+      "cw1_welsh" => "lib/cw1-form-welsh-CCQoct24.pdf",
+      "cw2_welsh" => "lib/cw2imm-form-welsh-CCQoct24.pdf",
+      "cw5_welsh" => "lib/cw5-form-welsh-CCQoct24.pdf",
+      "cw1_and_2_welsh" => "lib/cw1-form-welsh-CCQoct24.pdf",
+      "civ_means_7_welsh" => "lib/civ-means-7-form-welsh-CCQoct24.pdf",
     }.freeze
 
     def template_path(form_key)
@@ -45,18 +38,6 @@ class ControlledWorkDocumentPopulationService
     def values(session_data, form_key)
       mappings = YAML.load_file(Rails.root.join("app/lib/controlled_work_mappings/#{form_key}.yml")).map(&:with_indifferent_access)
       ControlledWorkDocumentValueMappingService.call(session_data, mappings)
-    end
-
-    def generate_form_key(model)
-      form_key = model.form_type
-      form_key += "_welsh" if model.language == "welsh"
-
-      # when the flag is enabled we check the form_type to determine whether form_key has the _new suffix
-      if FeatureFlags.enabled?(:cw_form_updates, without_session_data: true) && (model.form_type == "cw1" || model.form_type == "civ_means_7")
-        form_key += "_new"
-      end
-
-      form_key
     end
   end
 end
