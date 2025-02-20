e4f8db4e73ddc7993ec3f7a8d7df882a55be3e03
MazOneTwoOne <76905544+MazOneTwoOne@users.noreply.github.com>
2025-02-20 09:43:54 +0000

    If applied, this change will add a test and add correct link text

--- spec/forms/asylum_support_form_spec.rb
+++ spec/forms/asylum_support_form_spec.rb
@@ -24,6 +24,7 @@ RSpec.describe "asylum_support", type: :feature do
 
   it "has no extra piece of guidance" do
     expect(page).not_to have_content "Controlled work"
+    expect(page).not_to have_content "If your client does not have evidence of Asylum Support"
   end
 
   context "when in a controlled work context" do
@@ -31,6 +32,10 @@ RSpec.describe "asylum_support", type: :feature do
 
     it "has an extra piece of guidance" do
       expect(page).to have_content "Controlled work"
+    end
+
+    it "has details summary component" do
+      expect(page).to have_content "If your client does not have evidence of Asylum Support"
     end
   end
 end
