61136b6793f083c70df9860b134ee2f8a2689a38
MazOneTwoOne <76905544+MazOneTwoOne@users.noreply.github.com>
2025-02-26 15:10:37 +0000

    If applied, this change will validate the `event_type` before saving to AnalyticsEvent table: - have also added a test

--- app/services/analytics_service.rb
+++ app/services/analytics_service.rb
@@ -1,4 +1,8 @@
 class AnalyticsService
+  ALLOWED_EVENT_TYPE = YAML.load_file(
+    Rails.root.join("config/allowed_analytics_event_types.yml"),
+  )["allowed_event_types"].freeze
+
   ALLOWED_PAGES = YAML.load_file(
     Rails.root.join("config/allowed_analytics_pages.yml"),
   )["allowed_pages"].freeze
@@ -6,6 +10,7 @@ class AnalyticsService
   class << self
     def call(event_type:, page:, assessment_code:, cookies:)
       return if cookies[CookiesController::NO_ANALYTICS_MODE]
+      return unless valid_event_type?(event_type)
       return unless valid_page?(page)
 
       browser_id = cookies[ApplicationController::BROWSER_ID_COOKIE]
@@ -20,6 +25,10 @@ class AnalyticsService
     end
 
   private
+
+    def valid_event_type?(event_type)
+      ALLOWED_EVENT_TYPE.include?(event_type.to_s)
+    end
 
     def valid_page?(page)
       ALLOWED_PAGES.include?(page.to_s)
--- spec/services/analytics_service_spec.rb
+++ spec/services/analytics_service_spec.rb
@@ -5,26 +5,27 @@ RSpec.describe AnalyticsService do
     let(:assessment_code) { "just-another-assessment-code" }
 
     it "handles errors without crashing", :throws_cfe_error do
-      allow(described_class).to receive(:valid_page?).and_return(true)
+      allow(described_class).to receive_messages(valid_event_type?: true, valid_page?: true)
       allow(AnalyticsEvent).to receive(:create!).and_raise(StandardError)
       expect(ErrorService).to receive(:call)
 
-      expect { described_class.call(event_type: nil, page: "some_page", assessment_code: assessment_code, cookies: {}) }.not_to raise_error
+      expect { described_class.call(event_type: "an_event_type", page: "some_page", assessment_code: assessment_code, cookies: {}) }.not_to raise_error
     end
 
     it "respects no-analytics mode" do
       cookies = { CookiesController::NO_ANALYTICS_MODE => "true" }
-      described_class.call(event_type: nil, page: "some_page", assessment_code: assessment_code, cookies:)
+      described_class.call(event_type: "an_event_type", page: "some_page", assessment_code: assessment_code, cookies:)
       expect(AnalyticsEvent.count).to eq 0
     end
 
-    it "creates an analytics event for whitelisted page" do
+    it "creates an analytics event for allowlisted page & event_type" do
       cookies = { ApplicationController::BROWSER_ID_COOKIE => "browser123" }
+      event_type = "click_lc_guidance_controlled"
       page = "client_age"
 
       expect {
         described_class.call(
-          event_type: "click",
+          event_type: event_type,
           page: page,
           assessment_code: assessment_code,
           cookies: cookies,
@@ -32,13 +33,14 @@ RSpec.describe AnalyticsService do
       }.to change(AnalyticsEvent, :count).by(1)
     end
 
-    it "does not create an analytics event for non-whitelisted page" do
+    it "does not create an analytics event for non-allowlisted page & event_type" do
       cookies = { ApplicationController::BROWSER_ID_COOKIE => "browser123" }
+      event_type = "click_lc_guidance_controlled_0'XOR(if(now()=sysdate(),sleep(15),0))XOR'Z"
       page = "not-property_entry"
 
       expect {
         described_class.call(
-          event_type: "click",
+          event_type: event_type,
           page: page,
           assessment_code: assessment_code,
           cookies: cookies,
