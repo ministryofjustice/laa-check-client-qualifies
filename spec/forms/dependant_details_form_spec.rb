729a840da1478ddce5ecd4b64b3156558f120247
Will Clarke <william.clarke@digital.justice.gov.uk>
2025-03-19 14:00:12 +0000

    add specs for new content

--- spec/forms/dependant_details_form_spec.rb
+++ spec/forms/dependant_details_form_spec.rb
@@ -31,11 +31,13 @@ RSpec.describe "dependant_details", type: :feature do
     expect(session_contents["adult_dependants_count"]).to eq 1
   end
 
+  # This test will fail after the 7th April when the new thresholds come into force.
+  # To fix this test, change the value `361.70` on line 40 below to `367.87`
   it "shows me the dependant allowance text" do
     expect(page).to have_content(
       "Do not include:\n"\
-      "anyone who owns any property, vehicles or other assets valued at over £8,000 in total"\
-      "anyone with £361.70 or more income every month (any income below this can be entered on the following pages and will be deducted from the dependant allowance)",
+        "anyone who owns any property, vehicles or other assets valued at over £8,000 in total"\
+        "anyone with £361.70 or more income every month (any income below this can be entered on the following pages and will be deducted from the dependant allowance)",
     )
   end
 end
