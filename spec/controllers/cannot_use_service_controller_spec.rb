44da1d02d53d1711197237414aaa1fbeb7f2e443
Katharine Ahern <katharine.ahern@justice.gov.uk>
2025-03-27 08:54:55 +0000

    EL-2166: add controller spec

---
+++ spec/controllers/cannot_use_service_controller_spec.rb
@@ -0,0 +1,21 @@
+require "rails_helper"
+
+RSpec.describe CannotUseServiceController, type: :controller do
+  let(:assessment_code) { "123456" }
+  let(:session_data) { { "some" => "data", "assessment_code" => assessment_code } }
+
+  before do
+    allow(controller).to receive(:session_data).and_return(session_data)
+  end
+
+  describe "GET #additional_properties" do
+    it "returns success" do
+      expect(response).to have_http_status(200)
+    end
+
+    it "renders the additional properties template" do
+      get :additional_properties, params: { assessment_code: }
+      expect(response).to render_template("additional_properties")
+    end
+  end
+end
