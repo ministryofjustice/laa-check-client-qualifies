54d099a39ac21090fc3a5d6d382d019763e2b6f7
Katharine Ahern <katharine.ahern@justice.gov.uk>
2025-03-27 15:55:19 +0000

    EL-2166: add satisfaction feedback to footer

--- app/controllers/cannot_use_service_controller.rb
+++ app/controllers/cannot_use_service_controller.rb
@@ -1,4 +1,6 @@
 class CannotUseServiceController < ApplicationController
+  before_action :specify_satisfaction_feedback_page_name
+
   def additional_properties
     @check = Check.new(session_data)
     @previous_step = previous_step
@@ -12,5 +14,13 @@ private
 
   def previous_step
     params[:step]
+  end
+
+  def specify_feedback_widget
+    @feedback = :satisfaction
+  end
+
+  def specify_satisfaction_feedback_page_name
+    @satisfaction_feedback_page_name = "cannot_use_service_#{previous_step}"
   end
 end
