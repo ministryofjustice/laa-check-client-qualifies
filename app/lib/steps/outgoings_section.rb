b6bf19e75006f820dacb3131d8e5adc0e7431042
Will Clarke <william.clarke@digital.justice.gov.uk>
2025-04-01 23:33:19 +0100

    add specs which cover the affected flows and comments on the issue

--- app/lib/steps/outgoings_section.rb
+++ app/lib/steps/outgoings_section.rb
@@ -2,7 +2,7 @@ module Steps
   class OutgoingsSection
     class << self
       def all_steps
-        %i[outgoings partner_outgoings property property_landlord cannot_use_service shared_ownership_housing_costs mortgage_or_loan_payment housing_costs]
+        %i[outgoings partner_outgoings property property_landlord cannot_use_service shared_ownership_housing_costs mortgage_or_loan_payment housing_costs property_entry]
       end
 
       def grouped_steps_for(session_data)
@@ -29,14 +29,28 @@ module Steps
       end
 
       def housing_costs_group(session_data)
-        step = if session_data["property_owned"] == "shared_ownership"
-                 :shared_ownership_housing_costs
-               elsif Steps::Logic.owns_property_with_mortgage_or_loan?(session_data)
-                 :mortgage_or_loan_payment
-               elsif !Steps::Logic.owns_property_outright?(session_data)
-                 :housing_costs
-               end
-        Steps::Group.new(step) if step
+        # step = if session_data["property_owned"] == "shared_ownership"
+        #          :shared_ownership_housing_costs
+        #        elsif Steps::Logic.owns_property_with_mortgage_or_loan?(session_data)
+        #          :mortgage_or_loan_payment
+        #        elsif !Steps::Logic.owns_property_outright?(session_data)
+        #          :housing_costs
+        #        end
+        # Steps::Group.new(step) if step
+        steps = if session_data["property_owned"] == "shared_ownership"
+                  # the line below ensures that this test passes
+                  # spec/flows/shared_ownership_flow_spec.rb
+                  # %i[shared_ownership_housing_costs]
+                  # the line below ensures that this test passes
+                  # spec/flows/change_answers_flow_spec.rb:123
+                  %i[shared_ownership_housing_costs property_entry]
+                  # cant have both lines so how to fix this as property_entry is required across 2 sections
+                elsif Steps::Logic.owns_property_with_mortgage_or_loan?(session_data)
+                  [:mortgage_or_loan_payment]
+                elsif !Steps::Logic.owns_property_outright?(session_data)
+                  [:housing_costs]
+                end
+        Steps::Group.new(*steps) if steps
       end
     end
   end
