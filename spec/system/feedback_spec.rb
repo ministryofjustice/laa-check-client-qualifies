1ecca35a348e8eea50b4df8d2d628654ded1122f
Katharine Ahern <katharine.ahern@justice.gov.uk>
2025-04-09 16:59:47 +0100

    add intentionally failing test

--- spec/system/feedback_spec.rb
+++ spec/system/feedback_spec.rb
@@ -12,6 +12,7 @@ RSpec.describe "Feedback component" do
   describe "satisfaction feedback" do
     context "when on the end of journey page" do
       it "I can successfully submit satisfaction feedback", :slow do
+        raise "this is an intentional failure"
         start_assessment
         fill_in_client_age_screen
         fill_in_level_of_help_screen(choice: "Civil controlled work or family mediation")
