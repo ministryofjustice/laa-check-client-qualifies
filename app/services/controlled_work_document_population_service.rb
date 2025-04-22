2c11836669ebb253d1898ad80a4e45141ab40c89
MazOneTwoOne <76905544+MazOneTwoOne@users.noreply.github.com>
2025-04-22 09:56:47 +0100

    If applied, this adds a test for test coverage

--- app/services/controlled_work_document_population_service.rb
+++ app/services/controlled_work_document_population_service.rb
@@ -16,11 +16,8 @@ class ControlledWorkDocumentPopulationService
         file_name = "#{dir}/output-form.pdf"
         pdftk = PdfForms.new(`which pdftk`.chomp)
         pdftk.fill_form template_path(form_key), filled_path, values(session_data, form_key)
-        # Simplecov is showing that this branch is not covered, but there are tests to look at this
-        # :nocov:
         if Steps::Logic.owns_property_shared_ownership?(session_data)
           pdftk.call_pdftk filled_path, "background", background_path, "output", file_name
-        # :nocov:
         else
           FileUtils.cp filled_path, file_name
         end
---
+++ spec/services/controlled_work_document_population_service_spec.rb
@@ -0,0 +1,53 @@
+require "rails_helper"
+
+RSpec.describe ControlledWorkDocumentPopulationService do
+  describe ".call" do
+    let(:pdftk_instance) { double("PdfForms", fill_form: true, call_pdftk: true) }
+    let(:template_path) { "template/path.pdf" }
+    let(:filled_path) { /filled-form\.pdf$/ }
+    let(:background_path) { "lib/CWforms_SharedOwnership_english.pdf" }
+    let(:file_name) { /output-form\.pdf$/ }
+    let(:session_data) { { "property_owned" => property_status } }
+
+    let(:model) do
+      double(
+        form_type: "SharedOwnership",
+        language: "english",
+      )
+    end
+
+    before do
+      allow(PdfForms).to receive(:new).and_return(pdftk_instance)
+      allow(described_class).to receive(:template_path).and_return(template_path)
+      allow(described_class).to receive(:values).and_return({})
+      allow(File).to receive(:read).and_return("PDF_CONTENT")
+      allow(FileUtils).to receive(:cp).and_return(true)
+    end
+
+    context "when shared ownership" do
+      let(:property_status) { "shared_ownership" }
+
+      before do
+        allow(Steps::Logic).to receive(:owns_property_shared_ownership?).with(session_data).and_return(true)
+      end
+
+      it "calls pdftk with background_path" do
+        expect(pdftk_instance).to receive(:call_pdftk).with(filled_path, "background", background_path, "output", file_name)
+        described_class.call(session_data, model) { |_| }
+      end
+    end
+
+    context "when property owned outright" do
+      let(:property_status) { "outright" }
+
+      before do
+        allow(Steps::Logic).to receive(:owns_property_shared_ownership?).with(session_data).and_return(false)
+      end
+
+      it "does not call pdftk#call_pdftk" do
+        expect(pdftk_instance).not_to receive(:call_pdftk)
+        described_class.call(session_data, model) { |_| }
+      end
+    end
+  end
+end
