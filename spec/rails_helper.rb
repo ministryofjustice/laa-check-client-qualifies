5fe68dcb0ca9a4db3bd621d9f8050c9fa4c82cb9
Masum Khan <masum.khan@digital.justice.gov.uk>
2025-04-29 17:19:34 +0100

    If applied, this will resolve failing local tests, by using a gem to synchronize Capybara commands with client-side JavaScript.

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -15,6 +15,7 @@ require "database_cleaner/active_record"
 Capybara.register_driver :headless_chrome do |app|
   options = Selenium::WebDriver::Chrome::Options.new
   options.add_argument("--headless=new")
+  options.add_argument("unhandled_prompt_behavior: 'ignore'")
   Capybara::Selenium::Driver.new(app, browser: :chrome, options:)
 end
 
