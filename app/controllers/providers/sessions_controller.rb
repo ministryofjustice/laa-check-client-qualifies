6bc2be6eb27c9caddddd0af22e15a944682f1d24
Masum Khan <masum.khan@digital.justice.gov.uk>
2025-08-26 16:53:43 +0100

    If applied, this removes the bulk of the CCQ and Portal integration

--- app/controllers/providers/sessions_controller.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-module Providers
-  class SessionsController < Devise::SessionsController
-    def after_sign_out_path_for(_resource_name)
-      start_portal_signed_out_path
-    end
-  end
-end
