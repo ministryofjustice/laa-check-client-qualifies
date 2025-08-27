8343eb92d3b9c1c7fcf5bcd305721dfbd90ac0db
Masum Khan <masum.khan@digital.justice.gov.uk>
2025-08-27 15:17:38 +0100

    This fixes the omniauth path
    If applied this;
    - updates the matching of routes so that user is taken to correct path.
    - updates the uri path for the `subdomain_redirect` route and adds in some tests

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -302,7 +302,7 @@ Devise.setup do |config|
   #
   # When using OmniAuth, Devise cannot automatically set OmniAuth path,
   # so you need to do it manually. For the users scope, it would be:
-  # config.omniauth_path_prefix = '/my_engine/users/auth'
+  config.omniauth_path_prefix = "/admins/auth"
 
   # ==> Hotwire/Turbo configuration
   # When using Devise with Hotwire/Turbo, the http status for error responses
