f5bb10c589e485649f6c19ff0bc1e1a78e0b90f9
Sandy Ryalls <sandy.ryalls@justice.gov.uk>
2025-09-25 18:08:30 +0100

    use app DP for caching

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_08_27_102123) do
+ActiveRecord::Schema[7.2].define(version: 2025_09_25_170249) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -111,9 +111,6 @@ ActiveRecord::Schema[7.2].define(version: 2025_08_27_102123) do
     t.index ["assessment_id"], name: "index_completed_user_journeys_on_assessment_id"
   end
 
-  create_table "data_migrations", primary_key: "version", id: :string, force: :cascade do |t|
-  end
-
   create_table "feature_flag_overrides", force: :cascade do |t|
     t.string "key"
     t.boolean "value"
@@ -154,6 +151,17 @@ ActiveRecord::Schema[7.2].define(version: 2025_08_27_102123) do
     t.datetime "updated_at", null: false
     t.text "comment"
     t.string "page"
+  end
+
+  create_table "solid_cache_entries", force: :cascade do |t|
+    t.binary "key", null: false
+    t.binary "value", null: false
+    t.datetime "created_at", null: false
+    t.bigint "key_hash", null: false
+    t.integer "byte_size", null: false
+    t.index ["byte_size"], name: "index_solid_cache_entries_on_byte_size"
+    t.index ["key_hash", "byte_size"], name: "index_solid_cache_entries_on_key_hash_and_byte_size"
+    t.index ["key_hash"], name: "index_solid_cache_entries_on_key_hash", unique: true
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
