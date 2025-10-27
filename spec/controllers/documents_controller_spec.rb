bf68097959ea9f26deb4619024b6353928db1f84
Sandy Ryalls <sandy.ryalls@justice.gov.uk>
2025-10-27 10:29:04 +0000

    adding protection from probing

--- spec/controllers/documents_controller_spec.rb
+++ spec/controllers/documents_controller_spec.rb
@@ -31,5 +31,29 @@ RSpec.describe DocumentsController, type: :controller do
       expect(AnalyticsEvent.find_by(event_type: "click_lc_guidance_controlled", assessment_code: "foo")).to be_nil
       expect(response.headers["Location"]).to eq "https://assets.publishing.service.gov.uk/media/684ff44229fb1002010c4e6c/LC_s_guidance_on_determining_financial_eligibility__controlled_and_mediation_.pdf"
     end
+
+    context "when handling invalid document IDs" do
+      it "returns 404 for unknown document IDs (e.g., bot probes)" do
+        get :show, params: { id: "wp.php" }
+        expect(response).to have_http_status(:not_found)
+      end
+
+      it "returns 404 for other common exploit attempts" do
+        %w[admin.php index.php bypass.php alfa.php k.php].each do |malicious_id|
+          get :show, params: { id: malicious_id }
+          expect(response).to have_http_status(:not_found)
+        end
+      end
+
+      it "does not track analytics for invalid document IDs" do
+        get :show, params: { id: "wp.php", assessment_code: "foo", referrer: "some_page" }
+        expect(AnalyticsEvent.find_by(assessment_code: "foo")).to be_nil
+      end
+
+      it "returns 404 for invalid subsections" do
+        get :show, params: { id: "lc_guidance_controlled", sub_section: "nonexistent" }
+        expect(response).to have_http_status(:not_found)
+      end
+    end
   end
 end
