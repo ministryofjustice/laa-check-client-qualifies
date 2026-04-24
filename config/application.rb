aace9b1940b79120c921a252ac43cf9eb40776fd
Kyle Jones <kyle.jones2@justice.gov.uk>
2026-04-24 10:18:47 +0100

    feat: :sparkles: Adding ModeConfig class

--- config/application.rb
+++ config/application.rb
@@ -1,15 +1,18 @@
 require_relative "boot"
+require_relative "../lib/mode_config"
 
 require "rails"
 # Pick the frameworks you want:
 require "active_model/railtie"
 # require "active_job/railtie"
 require "active_record/railtie"
-require "active_storage/engine"
 require "action_controller/railtie"
 # require "action_mailer/railtie"
 # require "action_mailbox/engine"
-require "action_text/engine"
+unless ModeConfig.embedded?
+  require "active_storage/engine"
+  require "action_text/engine"
+end
 require "action_view/railtie"
 # require "action_cable/engine"
 require "rails/test_unit/railtie"
