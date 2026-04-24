f3d9b225f7e760b2c025b888708b54939fc3ddec
Kyle Jones <kyle.jones2@justice.gov.uk>
2026-04-24 13:57:21 +0100

    feat: :sparkles: Adding conditional for embedded mode around cache type

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -25,7 +25,15 @@ Rails.application.configure do
   # Show full error reports and disable caching.
   config.consider_all_requests_local = true
   config.action_controller.perform_caching = false
-  config.cache_store = :solid_cache_store
+  config.cache_store = if ModeConfig.embedded?
+                         [:redis_cache_store,
+                          {
+                            url: ENV["REDIS_URL"],
+                            namespace: "ccq",
+                          }]
+                       else
+                         :solid_cache_store
+                       end
   config.session_store :cache_store, key: LaaEstimateFinancialEligibilityForLegalAid::SESSION_COOKIE_NAME
 
   # Raise exceptions instead of rendering exception templates.
