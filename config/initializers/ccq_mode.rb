b50d67230e8f5926684318a1396682ffeb9483f1
Richard Cane <29371470+richardcane@users.noreply.github.com>
2026-05-01 18:34:46 +0100

    Add ccq_mode initializer for logging the boot mode

---
+++ config/initializers/ccq_mode.rb
@@ -0,0 +1,12 @@
+return if Rails.env.test?
+
+Rails.application.config.after_initialize do
+  mode_name = ModeConfig.embedded? ? "EMBEDDED (Redis)" : "STANDALONE (Solid Cache)"
+  
+  banner = "\n========================================\n" \
+           "🚀 BOOTING IN #{mode_name} MODE\n" \
+           "========================================\n"
+
+  puts banner
+  Rails.logger.info(banner)
+end
