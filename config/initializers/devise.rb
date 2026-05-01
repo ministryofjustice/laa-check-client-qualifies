f53d0d8151ecb294812201f61f8b95060f006a22
Richard Cane <29371470+richardcane@users.noreply.github.com>
2026-05-01 17:45:20 +0100

    Remove guard from Admin model, add it to devise initializer

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+return unless ModeConfig.oauth_enabled?
+
 # Assuming you have not yet modified this file, each configuration option below
 # is set to its default value. Note that some are commented out while others
 # are not: uncommented lines are intended to protect your configuration from
@@ -8,6 +10,7 @@
 #
 # Use this hook to configure devise mailer, warden hooks and so forth.
 # Many of these configuration options can be set straight in your model.
+
 Devise.setup do |config|
   # The secret key used by Devise. Devise uses this key to generate
   # random tokens. Changing this key will render invalid all existing
