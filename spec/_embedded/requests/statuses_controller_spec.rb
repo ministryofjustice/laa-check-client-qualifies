77910bb3a6ecf6c1b32264b627ae2f758b36263a
Richard Cane <29371470+richardcane@users.noreply.github.com>
2026-05-02 18:37:51 +0100

    Add status controller spec for embedded mode

---
+++ spec/_embedded/requests/statuses_controller_spec.rb
@@ -0,0 +1,39 @@
+require "rails_helper"
+
+RSpec.describe "status requests", type: :request do
+  let(:response_json) { JSON.parse(response.body) }
+
+  describe "GET /status" do
+    it "is successful and alive returns true" do
+      get("/status")
+      expect(response).to be_successful
+      expect(response_json).to eq("alive" => true)
+    end
+  end
+
+  describe "GET /health" do
+    it "is successful and healthy returns true" do
+      get("/health")
+      expect(response).to be_successful
+      expect(response_json).to eq("healthy" => true)
+    end
+
+    it "returns false if there is a problem writing to the cache" do
+      allow(Rails.cache).to receive(:write).and_return(false)
+      get("/health")
+      expect(response).not_to be_successful
+    end
+
+    it "returns false if reading from the cache raise an error" do
+      allow(Rails.cache).to receive(:read).and_raise(StandardError)
+      get("/health")
+      expect(response).not_to be_successful
+    end
+
+    it "returns false if there is a problem reading from the cache" do
+      allow(Rails.cache).to receive(:read).and_return(nil)
+      get("/health")
+      expect(response).not_to be_successful
+    end
+  end
+end
