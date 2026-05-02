be8fd06305f56947b4c756753e0770d8dd867c2d
Richard Cane <29371470+richardcane@users.noreply.github.com>
2026-05-02 22:41:54 +0100

    Revert environment logging

--- config/environment.rb
+++ config/environment.rb
@@ -1,18 +1,5 @@
 # Load the Rails application.
 require_relative "application"
 
-ActiveSupport::Notifications.subscribe("run_initializer.rails") do |_, _, _, _, payload|
-  # Print the name of every initializer as it runs
-  warn "=================================================="
-  warn "Running initializer: #{payload[:initializer].name}"
-end
-
-begin
-  # Initialize the Rails application.
-  Rails.application.initialize!
-rescue FrozenError => e
-  warn e.backtrace
-  warn "================ FROZEN ERROR ======================"
-
-  raise e
-end
+# Initialize the Rails application.
+Rails.application.initialize!
