cb947b384de95fce74995b180716e0d7fdc2f09f
Richard Cane <29371470+richardcane@users.noreply.github.com>
2026-05-02 22:28:15 +0100

    Add logging to environment for initializers

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,18 @@
 # Load the Rails application.
 require_relative "application"
 
-# Initialize the Rails application.
-Rails.application.initialize!
+ActiveSupport::Notifications.subscribe("run_initializer.rails") do |_, _, _, _, payload|
+  # Print the name of every initializer as it runs
+  warn "=================================================="
+  warn "Running initializer: #{payload[:initializer].name}"
+end
+
+begin
+  # Initialize the Rails application.
+  Rails.application.initialize!
+rescue FrozenError => e
+  warn e.backtrace
+  warn "================ FROZEN ERROR ======================"
+
+  raise e
+end
