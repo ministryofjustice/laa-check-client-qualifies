34698db3c1a4f91a8348b15632435a3d1acf1234
Kyle Jones <kyle.jones2@justice.gov.uk>
2026-05-18 15:16:54 +0100

    feat: :sparkles: Adding embedded_checks_controller functional code

---
+++ app/controllers/embedded_checks_controller.rb
@@ -0,0 +1,19 @@
+class EmbeddedChecksController < EmbeddedBaseController
+  include QuestionFlowMethods
+
+  before_action :clear_early_result, only: :check_answers
+
+  def check_answers
+    @check = Check.new(session_data)
+    @previous_step = Steps::Helper.last_step(session_data)
+    @sections = CheckAnswers::SectionListerService.call(session_data)
+    track_page_view(page: :check_answers)
+    render "checks/check_answers"
+  end
+
+private
+
+  def clear_early_result
+    session_data["early_result"]&.clear
+  end
+end
