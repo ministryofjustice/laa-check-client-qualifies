e2bd91a6965a355d1bc0588609399113b376579f
Kyle Jones <kyle.jones2@justice.gov.uk>
2026-05-18 15:17:07 +0100

    test: :test_tube: Adding embedded_forms_controller unit tests

---
+++ spec/controllers/embedded_forms_controller_spec.rb
@@ -0,0 +1,14 @@
+# RSpec.describe EmbeddedFormsController, type: :controller do
+#   let(:resource_id) { "test_resource_id" }
+#   let(:session_data) { { "key" => "value" } }
+#   let(:controller) { described_class.new.tap { |c| c.params = { resource_id: resource_id } } }
+#   let(:journey_store) { instance_double(JourneyDataStore::SessionStore) }
+
+#   before do
+#     allow(JourneyDataStore::SessionStore).to receive(:new).with(session, anything).and_return(journey_store)
+#     allow(journey_store).to receive(:read).and_return(session_data)
+#   end
+
+#   describe "GET #show" do
+#   end
+# end
