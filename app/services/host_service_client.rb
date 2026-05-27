4910fce0b0967e67da202b0e70239e7d3ad7302c
Kyle Jones <kyle.jones2@justice.gov.uk>
2026-05-27 16:14:26 +0100

    feat: :sparkles: Adding Host Service client

---
+++ app/services/host_service_client.rb
@@ -0,0 +1,33 @@
+class HostServiceClient
+  class ConnectionError < StandardError; end
+
+  CONNECT_TIMEOUT = 2
+  READ_TIMEOUT = 5
+
+  def load(resource_id:, cookies:)
+    post("/api/private/load", { resource_id: }, cookies:)
+  end
+
+  def save(resource_id:, result:, cookies:)
+    post("/api/private/save", { resource_id:, result: }, cookies:)
+  end
+
+private
+
+  def post(path, body, cookies:)
+    connection(cookies:).post(path, body)
+  rescue Faraday::ConnectionFailed, Faraday::TimeoutError => e
+    raise ConnectionError, e.message
+  end
+
+  def connection(cookies:)
+    Faraday.new(url: ENV.fetch("HOST_SERVICE_URL")) do |faraday|
+      faraday.options.open_timeout = CONNECT_TIMEOUT
+      faraday.options.timeout = READ_TIMEOUT
+      faraday.headers["Cookie"] = cookies if cookies.present?
+      faraday.request :json
+      faraday.response :json
+      faraday.adapter :net_http_persistent
+    end
+  end
+end
